61b8119c062062bffe78f09ffce623e2cf4297bf
Manish R Jain <manish@dgraph.io>
2017-01-26 18:17:18 +1100

    Add a value log

---
+++ value/value.go
@@ -0,0 +1,107 @@
+package value
+
+import (
+	"bytes"
+	"encoding/binary"
+	"io"
+	"os"
+
+	"github.com/dgraph-io/dgraph/x"
+	"github.com/pkg/errors"
+)
+
+type Log struct {
+	x.SafeMutex
+	fd *os.File
+}
+
+type Entry struct {
+	Key   []byte
+	Value []byte
+}
+
+type header struct {
+	active byte
+	klen   uint32
+	vlen   uint32
+}
+
+func (h header) Encode() []byte {
+	b := make([]byte, 1+4+4)
+	b[0] = byte(0)
+	binary.BigEndian.PutUint32(b[1:5], h.klen)
+	binary.BigEndian.PutUint32(b[5:9], h.vlen)
+	return b
+}
+
+func (h *header) Decode(buf []byte) []byte {
+	h.active = buf[0]
+	h.klen = binary.BigEndian.Uint32(buf[1:5])
+	h.vlen = binary.BigEndian.Uint32(buf[5:9])
+	return buf[9:]
+}
+
+type Pointer struct {
+	Len    uint32
+	Offset int64
+}
+
+func (l *Log) Open(fname string) {
+	var err error
+	l.fd, err = os.OpenFile(fname, os.O_RDWR|os.O_CREATE|os.O_SYNC, 0666)
+	x.Check(err)
+	_, err = l.fd.Write([]byte("test"))
+	x.Check(err)
+}
+
+func (l *Log) Write(entries []Entry) ([]Pointer, error) {
+	var buf bytes.Buffer
+	var h header
+
+	ptrs := make([]Pointer, 0, len(entries))
+	for _, e := range entries {
+		h.active = byte(0)
+		h.klen = uint32(len(e.Key))
+		h.vlen = uint32(len(e.Value))
+		header := h.Encode()
+
+		var p Pointer
+		p.Len = uint32(len(header)) + h.klen + h.vlen
+		p.Offset = int64(buf.Len())
+		ptrs = append(ptrs, p)
+
+		buf.Write(header)
+		buf.Write(e.Key)
+		buf.Write(e.Value)
+	}
+
+	l.Lock()
+	defer l.Unlock()
+
+	off, err := l.fd.Seek(0, io.SeekEnd)
+	if err != nil {
+		return ptrs, errors.Wrap(err, "Unable to seek")
+	}
+	for i := range ptrs {
+		p := &ptrs[i]
+		p.Offset += off
+	}
+
+	_, err = l.fd.Write(buf.Bytes())
+	return ptrs, errors.Wrap(err, "Unable to write to file")
+}
+
+func (l *Log) Read(offset int64, len uint32, fn func(Entry)) error {
+	var e Entry
+	buf := make([]byte, len)
+	if _, err := l.fd.ReadAt(buf, offset); err != nil {
+		return err
+	}
+	var h header
+	buf = h.Decode(buf)
+	e.Key = buf[0:h.klen]
+	buf = buf[h.klen:]
+	e.Value = buf[0:h.vlen]
+	fn(e)
+	return nil
+}
---
+++ value/value_test.go
@@ -0,0 +1,71 @@
+package value
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"testing"
+)
+
+func BenchmarkReadWrite(b *testing.B) {
+	rwRatio := []float32{
+		0.1, 0.2, 0.5, 1.0,
+	}
+	valueSize := []int{
+		64, 128, 256, 512, 1024, 2048, 4096,
+	}
+
+	for _, vsz := range valueSize {
+		for _, rw := range rwRatio {
+			b.Run(fmt.Sprintf("rw-%3.1f-valuesize:%04d", rw, vsz), func(b *testing.B) {
+				var vl Log
+				vl.Open("/tmp/vlog")
+				defer os.Remove("/tmp/vlog")
+				b.ResetTimer()
+
+				b.RunParallel(func(pb *testing.PB) {
+					var e Entry
+					e.Key = make([]byte, 16)
+					e.Value = make([]byte, vsz)
+
+					var ptrs []Pointer
+
+					pt, err := vl.Write([]Entry{e})
+					if err != nil {
+						b.Fatalf("Benchmark Write: ", err)
+					}
+					ptrs = append(ptrs, pt...)
+
+					for pb.Next() {
+						f := rand.Float32()
+						if f < rw {
+							pt, err := vl.Write([]Entry{e})
+							if err != nil {
+								b.Fatalf("Benchmark Write: ", err)
+							}
+							ptrs = append(ptrs, pt...)
+
+						} else {
+							ln := len(ptrs)
+							if ln == 0 {
+								b.Fatalf("Zero length of ptrs")
+							}
+							idx := rand.Intn(ln)
+							p := ptrs[idx]
+							if err := vl.Read(p.Offset, p.Len, func(e Entry) {
+								if len(e.Key) != 16 {
+									b.Fatalf("Key is invalid")
+								}
+								if len(e.Value) != vsz {
+									b.Fatalf("Value is invalid")
+								}
+							}); err != nil {
+								b.Fatalf("Benchmark Read:", err)
+							}
+						}
+					}
+				})
+			})
+		}
+	}
+}
