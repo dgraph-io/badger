030a485ccc79a12ff26016473a70ba4417dfcf86
Ganesh Acharya <ganeshacharya255@gmail.com>
2017-02-21 23:20:28 +0800

    Update memtable to use skiplist in skl package

--- memtable/memtable.go
+++ memtable/memtable.go
@@ -2,127 +2,59 @@ package memtable
 
 import (
 	//	"bytes"
-	"encoding/binary"
+	//	"encoding/binary"
 	//	"log"
 	"os"
+	"unsafe"
 
-	"github.com/dgraph-io/badger/skiplist"
+	"github.com/dgraph-io/badger/skl"
 	"github.com/dgraph-io/badger/table"
 	"github.com/dgraph-io/badger/y"
 )
 
-type KeyComparator struct {
-	userCmp skiplist.Comparator // For comparing user keys.
-}
-
+// Memtable is a thin wrapper over Skiplist, at least for now.
 type Memtable struct {
-	table *skiplist.Skiplist
-	cmp   KeyComparator // User key comparator.
+	table *skl.Skiplist
 	arena *y.Arena
 }
 
-func (s KeyComparator) Compare(a, b []byte) int {
-	// Read the length prefix to get internal key length.
-	// Grab "internal key length" many bytes.
-	// Compare user keys. If the same, compare the extra bits which comprise
-	// the sequence number and value type.
-	k1, _ := y.GetLengthPrefixedSlice(a)
-	k2, _ := y.GetLengthPrefixedSlice(b)
-	y.AssertTrue(len(k1) >= 8)
-	y.AssertTrue(len(k2) >= 8)
-
-	// Compare user keys. Remove the last 8 bytes.
-	u1 := k1[:len(k1)-8]
-	u2 := k2[:len(k2)-8]
-	r := s.userCmp.Compare(u1, u2) // Compare user keys.
-	if r != 0 {
-		return r
-	}
-	// User keys are equal. Compare the extra stuff.
-	// Decreasing sequence number, then decreasing value type.
-	// In big endian, this is easy.
-	e1 := binary.BigEndian.Uint64(k1[len(k1)-8:])
-	e2 := binary.BigEndian.Uint64(k2[len(k2)-8:])
-	if e1 > e2 {
-		return -1
-	} else if e1 < e2 {
-		return 1
-	}
-	return 0
-}
-
-var DefaultKeyComparator = KeyComparator{
-	userCmp: skiplist.DefaultComparator,
-}
+const (
+	byteData   = 0
+	byteDelete = 1
+)
 
 // NewMemtable creates a new memtable. Input is the user key comparator.
-func NewMemtable(cmp KeyComparator) *Memtable {
-	out := &Memtable{
-		cmp:   cmp,
+func NewMemtable() *Memtable {
+	return &Memtable{
 		arena: new(y.Arena),
-		table: skiplist.NewSkiplist(12, 4, cmp),
+		table: skl.NewSkiplist(),
 	}
-	// For now, just use some default values. Can be exposed as options later.
-	return out
 }
 
-func (s *Memtable) Add(seqNum uint64, typ y.ValueType, key []byte, value []byte) {
-	keySize := len(key)
-	valSize := len(value)
-	internalKeySize := keySize + 8
-
-	// buf1, buf2 should go on stack.
-	var buf1 [20]byte
-	//	buf1 := make([]byte, 8)
-	l1 := binary.PutUvarint(buf1[:], uint64(internalKeySize))
-
-	//	buf2 := make([]byte, 8)
-	var buf2 [20]byte
-	l2 := binary.PutUvarint(buf2[:], uint64(valSize))
-
-	//	out := make([]byte, l1+internalKeySize+l2+valSize)
-	out := s.arena.Allocate(l1 + internalKeySize + l2 + valSize)
-
-	// Internal key size.
-	y.AssertTrue(l1 == copy(out, buf1[:l1]))
-	p := out[l1:]
-
-	// User key.
-	y.AssertTrue(keySize == copy(p, key))
-	p = p[keySize:]
-
-	// Sequence number and value type.
-	binary.BigEndian.PutUint64(p, y.PackSeqAndType(seqNum, typ))
-	p = p[8:]
-
-	// Value size.
-	y.AssertTrue(l2 == copy(p, buf2[:l2]))
-	p = p[l2:]
-
-	// Value.
-	y.AssertTrue(valSize == copy(p, value))
-	p = p[valSize:]
-
-	y.AssertTrue(len(p) == 0)
-	s.table.Insert(out)
+// Put sets a key-value pair. We don't use onlyIfAbsent now. And we ignore the old value returned
+// by the skiplist. These can be used later on to support more operations, e.g., GetOrCreate can
+// be a put with an empty value with onlyIfAbsent=true.
+func (s *Memtable) Put(key, value []byte) {
+	data := s.arena.Allocate(1 + len(key) + len(value))
+	y.AssertTrue(len(key) == copy(data[:len(key)], key))
+	v := data[len(key):]
+	v[0] = byteData
+	y.AssertTrue(len(value) == copy(v[1:], value))
+	s.table.Put(data[:len(key)], unsafe.Pointer(&v), false)
 }
 
-// Encode a suitable internal key target for "target" and return it.
-// Uses *scratch as scratch space, and the returned pointer will point
-// into this scratch space.
-func encodeKey(key []byte) []byte {
-	var buf [20]byte
-	//	buf := make([]byte, 8)
-	l := binary.PutUvarint(buf[:], uint64(len(key)))
-	out := make([]byte, l+len(key)+1)
-	// Last byte is zero to denote valSize=0.
-	y.AssertTrue(l == copy(out, buf[:l]))
-	y.AssertTrue(len(key) == copy(out[l:], key))
-	return out
+// Delete deletes a key from the table.
+func (s *Memtable) Delete(key []byte) {
+	data := s.arena.Allocate(1 + len(key))
+	y.AssertTrue(len(key) == copy(data[:len(key)], key))
+	v := data[len(key):]
+	v[0] = byteDelete
+	s.table.Put(data[:len(key)], unsafe.Pointer(&v), false)
 }
 
+// WriteLevel0Table flushes memtable. It drops deleteValues.
 func (s *Memtable) WriteLevel0Table(f *os.File) error {
-	iter := s.Iterator()
+	iter := s.NewIterator()
 	b := table.TableBuilder{}
 	b.Reset()
 	for iter.SeekToFirst(); iter.Valid(); iter.Next() {
@@ -134,69 +66,41 @@ func (s *Memtable) WriteLevel0Table(f *os.File) error {
 	return nil
 }
 
+// Iterator is an iterator over memtable. It takes special care of deleteValue.
 type Iterator struct {
-	iter *skiplist.Iterator
+	iter *skl.Iterator
 }
 
-func (s *Memtable) Iterator() *Iterator {
-	return &Iterator{
-		iter: s.table.Iterator(),
-	}
+// NewIterator returns a memtable iterator.
+func (s *Memtable) NewIterator() *Iterator {
+	return &Iterator{iter: s.table.NewIterator()}
 }
 
-func (s *Iterator) Seek(internalKey []byte) {
-	s.iter.Seek(encodeKey(internalKey))
-}
+func (s *Iterator) Seek(key []byte) { s.iter.Seek(key) }
+func (s *Iterator) Valid() bool     { return s.iter.Valid() }
+func (s *Iterator) SeekToFirst()    { s.iter.SeekToFirst() }
+func (s *Iterator) SeekToLast()     { s.iter.SeekToLast() }
+func (s *Iterator) Next()           { s.iter.Next() }
+func (s *Iterator) Prev()           { s.iter.Prev() }
+func (s *Iterator) Key() []byte     { return s.iter.Key() }
 
-func (s *Iterator) Valid() bool  { return s.iter.Valid() }
-func (s *Iterator) SeekToFirst() { s.iter.SeekToFirst() }
-func (s *Iterator) SeekToLast()  { s.iter.SeekToLast() }
-func (s *Iterator) Next()        { s.iter.Next() }
-func (s *Iterator) Prev()        { s.iter.Prev() }
-
-// key returns the internal key, which is user key + seqNum + valueType.
-func (s *Iterator) Key() []byte {
-	k, _ := y.GetLengthPrefixedSlice(s.iter.Key())
-	return k
-}
-
+// Value returns the value and whether the key is deleted.
 func (s *Iterator) Value() []byte {
-	_, val := y.GetLengthPrefixedSlice(s.iter.Key())
-	valSlice, _ := y.GetLengthPrefixedSlice(val)
-	return valSlice
+	v := s.iter.Value()
+	y.AssertTrue(v != nil)
+	return *(*[]byte)(v)
 }
 
-// Get looks up a key. Returns the value and whether we found the key in this table.
-func (s *Memtable) Get(lkey *y.LookupKey) ([]byte, bool) {
-	if s == nil {
-		return nil, false // No hit.
-	}
-	it := s.Iterator()
-	it.Seek(lkey.InternalKey())
-	if !it.Valid() {
-		return nil, false // No hit.
-	}
+// IsDeleted returns whether the value returned denotes a deletion.
+func IsDeleted(v []byte) bool { return v[0] == byteDelete }
 
-	key := it.Key()
-	keyLen := len(key)
-
-	// Compare user keys.
-	if s.cmp.userCmp.Compare(lkey.UserKey(), key[:keyLen-8]) != 0 {
-		return nil, false // No hit.
+// Get looks up a key. Returns value and whether the key is explicitly deleted.
+func (s *Memtable) Get(key []byte) []byte {
+	v := s.table.Get(key)
+	if v == nil {
+		return nil
 	}
-
-	// Check value type.
-	tag := binary.BigEndian.Uint64(key[keyLen-8:])
-	typ := tag & 0xFF
-	switch typ {
-	case y.ValueTypeValue:
-		return it.Value(), true
-	case y.ValueTypeDeletion:
-		return nil, true // We have a hit, and we know this key is deleted.
-	default:
-		y.Fatalf("Unknown value type: %v", typ)
-	}
-	return nil, false // No hit.
+	return *(*[]byte)(v)
 }
 
 // MemUsage returns an approximate mem usage.
--- memtable/memtable_test.go
+++ memtable/memtable_test.go
@@ -6,108 +6,58 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/dgraph-io/badger/y"
+	//	"github.com/dgraph-io/badger/y"
 )
 
+func extract(m *Memtable) ([]string, []string) {
+	var keys, vals []string
+	it := m.NewIterator()
+	for it.SeekToFirst(); it.Valid(); it.Next() {
+		keys = append(keys, string(it.Key()))
+		v := it.Value()
+		if IsDeleted(v) {
+			vals = append(vals, "DEL")
+		} else {
+			vals = append(vals, string(v[1:]))
+		}
+	}
+	return keys, vals
+}
+
 func TestBasic(t *testing.T) {
-	m := NewMemtable(DefaultKeyComparator)
+	m := NewMemtable()
 	require.NotNil(t, m)
-	m.Add(123, y.ValueTypeValue, []byte("somekey"), []byte("hohoho"))
-	m.Add(120, y.ValueTypeValue, []byte("somekey"), []byte("hohoho"))
-}
+	m.Put([]byte("somekey"), []byte("hohoho"))
+	m.Put([]byte("somekey"), []byte("hahaha"))
+	k, v := extract(m)
+	require.EqualValues(t, []string{"somekey"}, k)
+	require.EqualValues(t, []string{"hahaha"}, v)
 
-func TestIterateAll(t *testing.T) {
-	m := NewMemtable(DefaultKeyComparator)
-	require.NotNil(t, m)
-	m.Add(123, y.ValueTypeValue, []byte("somekey"), []byte("hohoho1"))
-	m.Add(120, y.ValueTypeValue, []byte("somekey"), []byte("hohoho3"))
-	m.Add(200, y.ValueTypeValue, []byte("zzz"), []byte("hohoho4"))
-	m.Add(123, y.ValueTypeDeletion, []byte("somekey"), []byte("hohoho2"))
+	m.Delete([]byte("akey"))
+	m.Delete([]byte("somekey"))
+	k, v = extract(m)
+	require.EqualValues(t, []string{"akey", "somekey"}, k)
+	require.EqualValues(t, []string{"DEL", "DEL"}, v)
 
-	it := m.Iterator()
-	it.SeekToFirst()
-
-	require.True(t, it.Valid())
-	key := it.Key()
-	require.Len(t, key, 15)
-	require.EqualValues(t, "somekey", key[:7])
-	require.EqualValues(t, []byte{0, 0, 0, 0, 0, 0, 123, 1}, key[7:])
-	require.EqualValues(t, "hohoho1", it.Value())
-
-	it.Next()
-	require.True(t, it.Valid())
-	key = it.Key()
-	require.Len(t, key, 15)
-	require.EqualValues(t, "somekey", key[:7])
-	require.EqualValues(t, []byte{0, 0, 0, 0, 0, 0, 123, 0}, key[7:])
-	require.EqualValues(t, "hohoho2", it.Value())
-
-	it.Next()
-	require.True(t, it.Valid())
-	key = it.Key()
-	require.Len(t, key, 15)
-	require.EqualValues(t, "somekey", key[:7])
-	require.EqualValues(t, []byte{0, 0, 0, 0, 0, 0, 120, 1}, key[7:])
-	require.EqualValues(t, "hohoho3", it.Value())
-
-	it.Next()
-	require.True(t, it.Valid())
-	key = it.Key()
-	require.Len(t, key, 11)
-	require.EqualValues(t, "zzz", key[:3])
-	require.EqualValues(t, []byte{0, 0, 0, 0, 0, 0, 200, 1}, key[3:])
-	require.EqualValues(t, "hohoho4", it.Value())
-
-	it.Next()
-	require.False(t, it.Valid())
-}
-
-func TestGet(t *testing.T) {
-	m := NewMemtable(DefaultKeyComparator)
-	require.NotNil(t, m)
-	m.Add(223, y.ValueTypeValue, []byte("somekey"), []byte("hohoho"))
-	m.Add(123, y.ValueTypeDeletion, []byte("somekey"), []byte("aaa"))
-	m.Add(23, y.ValueTypeValue, []byte("somekey"), []byte("nonono"))
-	m.Add(200, y.ValueTypeValue, []byte("abckey"), []byte("bbb"))
-
-	v, hit := m.Get(y.NewLookupKey([]byte("abckey"), y.MaxSequenceNumber))
-	require.EqualValues(t, "bbb", v)
-	require.True(t, hit)
-
-	// Try a key that looks like existing keys.
-	v, hit = m.Get(y.NewLookupKey([]byte("somekeyaaa"), y.MaxSequenceNumber))
-	require.Nil(t, v)
-	require.False(t, hit)
-
-	// Try different sequence numbers to get different values.
-	v, hit = m.Get(y.NewLookupKey([]byte("somekey"), y.MaxSequenceNumber))
-	require.EqualValues(t, "hohoho", v)
-	require.True(t, hit)
-
-	v, hit = m.Get(y.NewLookupKey([]byte("somekey"), 200))
-	require.Nil(t, v)
-	require.True(t, hit) // There's a hit but key has been deleted.
-
-	v, hit = m.Get(y.NewLookupKey([]byte("somekey"), 100))
-	require.EqualValues(t, "nonono", v)
-	require.True(t, hit)
+	m.Put([]byte("somekey"), []byte("yes"))
+	k, v = extract(m)
+	require.EqualValues(t, []string{"akey", "somekey"}, k)
+	require.EqualValues(t, []string{"DEL", "yes"}, v)
 }
 
 func TestMemUssage(t *testing.T) {
-	m := NewMemtable(DefaultKeyComparator)
+	m := NewMemtable()
 	for i := 0; i < 10000; i++ {
-		m.Add(uint64(i), y.ValueTypeValue, []byte(fmt.Sprintf("k%05d", i)),
-			[]byte(fmt.Sprintf("v%05d", i)))
+		m.Put([]byte(fmt.Sprintf("k%05d", i)), []byte(fmt.Sprintf("v%05d", i)))
 	}
-	expected := 10000 * (8 + 6 + 1 + 1 + 6)
-	require.InEpsilon(t, expected, m.ApproximateMemoryUsage(), 0.1)
+	expected := 10000 * (6 + 6 + 1)
+	require.InEpsilon(t, expected, m.MemUsage(), 0.1)
 }
 
-// BenchmarkAdd-4   	 1000000	      1320 ns/op
+// BenchmarkAdd-4   	 1000000	      1289 ns/op
 func BenchmarkAdd(b *testing.B) {
-	m := NewMemtable(DefaultKeyComparator)
+	m := NewMemtable()
 	for i := 0; i < b.N; i++ {
-		m.Add(uint64(i), y.ValueTypeValue, []byte(fmt.Sprintf("k%09d", i)),
-			[]byte(fmt.Sprintf("v%09d", i)))
+		m.Put([]byte(fmt.Sprintf("k%09d", i)), []byte(fmt.Sprintf("v%09d", i)))
 	}
 }
