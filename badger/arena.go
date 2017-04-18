9c8c38fab1110909f421be48d5e9da0d2e823fcd
Ganesh Acharya <ganeshacharya255@gmail.com>
2017-04-19 00:29:26 +0800

    update

--- badger/arena.go
+++ badger/arena.go
@@ -1,51 +1,51 @@
-package badger
+//package badger
 
-import (
-	"sync"
+//import (
+//	"sync"
 
-	"github.com/dgraph-io/badger/skl"
-)
+//	"github.com/dgraph-io/badger/skl"
+//)
 
-type arenaList struct {
-	sync.Mutex // Guards used.
+//type arenaList struct {
+//	sync.Mutex // Guards used.
 
-	used []bool
+//	used []bool
 
-	arenas []*skl.Arena // Immutable.
-}
+//	arenas []*skl.Arena // Immutable.
+//}
 
-func newArenaList(size int64, numArenas int) *arenaList {
-	out := &arenaList{
-		arenas: make([]*skl.Arena, numArenas),
-		used:   make([]bool, numArenas),
-	}
-	for i := 0; i < numArenas; i++ {
-		out.arenas[i] = skl.NewArena(size)
-	}
-	return out
-}
+//func newArenaList(size int64, numArenas int) *arenaList {
+//	out := &arenaList{
+//		arenas: make([]*skl.Arena, numArenas),
+//		used:   make([]bool, numArenas),
+//	}
+//	for i := 0; i < numArenas; i++ {
+//		out.arenas[i] = skl.NewArena(size)
+//	}
+//	return out
+//}
 
-func (s *arenaList) getUnusedArena() *skl.Arena {
-	s.Lock()
-	defer s.Unlock()
-	for i, arena := range s.arenas {
-		if !s.used[i] {
-			s.used[i] = true
-			return arena
-		}
-	}
-	return nil
-}
+//func (s *arenaList) getUnusedArena() *skl.Arena {
+//	s.Lock()
+//	defer s.Unlock()
+//	for i, arena := range s.arenas {
+//		if !s.used[i] {
+//			s.used[i] = true
+//			return arena
+//		}
+//	}
+//	return nil
+//}
 
-func (s *arenaList) returnArena(arena *skl.Arena) bool {
-	// Do a simple loop. No need to do any hashing. There are very few arenas.
-	s.Lock()
-	defer s.Unlock()
-	for i, a := range s.arenas {
-		if arena == a {
-			s.used[i] = false
-			return true
-		}
-	}
-	return false
-}
+//func (s *arenaList) returnArena(arena *skl.Arena) bool {
+//	// Do a simple loop. No need to do any hashing. There are very few arenas.
+//	s.Lock()
+//	defer s.Unlock()
+//	for i, a := range s.arenas {
+//		if arena == a {
+//			s.used[i] = false
+//			return true
+//		}
+//	}
+//	return false
+//}
