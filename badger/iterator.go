b50cd8fea7346291e2e79dabeac44393ac1b89fd
Pawan Rawal <pawan0201@gmail.com>
2017-05-20 20:32:58 +1000

    Fix iterator pop bug in Seek (#38)

--- badger/iterator.go
+++ badger/iterator.go
@@ -191,7 +191,7 @@ func (it *Iterator) prefetch() {
 // greater than provided if iterating in the forward direction. Behavior would be reversed is
 // iterating backwards.
 func (it *Iterator) Seek(key []byte) {
-	for i := it.data.pop(); i != nil; {
+	for i := it.data.pop(); i != nil; i = it.data.pop() {
 		i.wg.Wait()
 		it.waste.push(i)
 	}
