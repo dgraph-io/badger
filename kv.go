e494fe8ee84236e92738aa7f4e476637491041b4
Sam Hughes <srh@dgraph.io>
2017-07-16 14:36:28 -0700

    Update comment about necessity of WR for Sync()

--- kv.go
+++ kv.go
@@ -357,7 +357,7 @@ func destroyPidFile(dir string) error {
 // When you create or delete a file, you have to ensure the directory entry for the file is synced
 // in order to guarantee the file is visible (if the system crashes).
 func syncDir(dir string) error {
-	// Is WR necessary for Sync()?  Whatever.
+	// Is WR necessary for Sync()?  Not on Linux, but I'm not going to test every platform.
 	f, err := os.OpenFile(dir, os.O_RDWR, 0666)
 	if err != nil {
 		return err
