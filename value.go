cf23d8dd823ddbac0ed8bb21d28e5893b8aa43c7
Janardhan Reddy <janardhan@dgraph.io>
2017-09-05 11:27:16 +1000

    update

--- value.go
+++ value.go
@@ -286,7 +286,8 @@ func (vlog *valueLog) rewrite(f *logFile) error {
 			ne := new(Entry)
 			if e.Meta == BitSetIfAbsent {
 				// If we rewrite this entry without removing BitSetIfAbsent, lsm would see that
-				// the key is already present and won't update the vptr to the new file.
+				// the key is already present, which would be this same entry and won't update
+				// the vptr to point to the new file.
 				e.Meta = 0
 			}
 			y.AssertTruef(e.Meta == 0, "Got meta: 0")
