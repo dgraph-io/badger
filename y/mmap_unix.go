70b6c2916551108b31f4e720608fab77e3a2dfc6
Deepak Jois <deepak.jois@gmail.com>
2017-09-11 09:18:42 +0530

    Use syscall interface to directly call madvise.
    It looks like the unix package we are using does not support madvise
    system call on OS X.
    
    Fixes #210.

--- y/mmap_unix.go
+++ y/mmap_unix.go
@@ -20,6 +20,8 @@ package y
 
 import (
 	"os"
+	"syscall"
+	"unsafe"
 
 	"golang.org/x/sys/unix"
 )
@@ -47,5 +49,14 @@ func Madvise(b []byte, readahead bool) error {
 	if !readahead {
 		flags = unix.MADV_RANDOM
 	}
-	return unix.Madvise(b, flags)
+	return madvise(b, flags)
+}
+
+// This is required because the unix package does not support the madvise system call on OS X.
+func madvise(b []byte, advice int) (err error) {
+	_, _, e1 := syscall.Syscall(syscall.SYS_MADVISE, uintptr(unsafe.Pointer(&b[0])), uintptr(len(b)), uintptr(advice))
+	if e1 != 0 {
+		err = e1
+	}
+	return
 }
