f8a55ddd33a9fefb299a110c0b7c665468e92eaa
Sam Hughes <srh@dgraph.io>
2017-09-14 19:06:56 -0700

    Backup store in the midst of being implemented

--- backupstore.go
+++ backupstore.go
@@ -17,55 +17,159 @@
 package badger
 
 import (
-	"encoding/binary"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
 
 	"github.com/dgraph-io/badger/protos"
-	"github.com/dgraph-io/badger/y"
 )
 
 const (
-	backupMagicText    string = "BACKBADG"
-	backupMagicVersion uint64 = 1
+	backupMagicText        string = "BACKBADG"
+	backupMagicVersion     uint64 = 1
+	backupManifestFilename        = "BACKUPMANIFEST"
 )
 
-// CreateBackupStore creates a new backup store in the given file.
+// CreateBackupStore creates a directory and initializes a new backup store in that directory.
 func CreateBackupStore(path string) (err error) {
-	f, err := y.CreateSyncedFile(path, false)
+	if err := os.Mkdir(path, 0755); err != nil {
+		return err
+	}
+	var initialStatus = protos.BackupStatus{
+		Backups: []*protos.BackupStatusItem{},
+	}
+	return writeBackupStatus(path, initialStatus)
+
+}
+
+func writeBackupStatus(path string, status protos.BackupStatus) (err error) {
+	// TODO: dir file syncing?  Locking?
+	data, err := status.Marshal()
+	if err != nil {
+		return err
+	}
+	f, err := ioutil.TempFile(path, "partial-manifest-")
 	if err != nil {
 		return err
 	}
 	defer func() {
-		if closeErr := f.Close(); err == nil {
-			err = closeErr
+		if f != nil {
+			// Error case hit already
+			_ = f.Close()
 		}
 	}()
+	tmpName := f.Name()
+	if f != nil {
 
-	// TODO: Flock file?  Flock everywhere?
-	buf := []byte{}
-	buf = append(buf, backupMagicText...)
-	var tmp [8]byte
-	binary.BigEndian.PutUint64(tmp[:], backupMagicVersion)
-	buf = append(buf, tmp[:]...)
-
-	if _, err := f.Write(buf); err != nil {
+	}
+	if _, err := f.Write(data); err != nil {
 		return err
 	}
-
+	if err := f.Sync(); err != nil {
+		return err
+	}
+	err = f.Close()
+	f = nil
+	if err != nil {
+		return err
+	}
+	if err := os.Rename(filepath.Join(path, tmpName),
+		filepath.Join(path, backupManifestFilename)); err != nil {
+		return err
+	}
 	return nil
+}
+
+var backupFilenameRegex = regexp.MustCompile("^backup-([1-9][0-9])+-([1-9][0-9]+)$")
+
+// ReadBackupStatus reads the BackupStatus description from the backup directory.
+func ReadBackupStatus(path string) (protos.BackupStatus, error) {
+	data, err := ioutil.ReadFile(filepath.Join(path, backupManifestFilename))
+	if err != nil {
+		return protos.BackupStatus{}, err
+	}
+	var ret protos.BackupStatus
+	if err := ret.Unmarshal(data); err != nil {
+		return protos.BackupStatus{}, err
+	}
+	return ret, nil
+}
+
+func backupFileName(id uint64) string {
+	return fmt.Sprintf("backup-%d", id)
 }
 
 // NewBackup stores a new backup onto an existing backup store.  itemCh is finished when it returns
 // an empty slice.
-func NewBackup(path string, itemCh <-chan []protos.BackupItem) error {
-	f, err := y.OpenExistingSyncedFile(path, false)
+func NewBackup(path string, maxCASCounter uint64, itemCh <-chan []protos.BackupItem) (err error) {
+	// TODO: Some sort of flocking to prevent multiple people from trying to add a backup
+	// simultaneously?
+	status, err := ReadBackupStatus(path)
 	if err != nil {
 		return err
 	}
+
+	maxBackupID := uint64(0)
+	for _, backup := range status.Backups {
+		if backup.MaxCASCounter > maxCASCounter {
+			return fmt.Errorf("backup %d exists with counter value %d (higher than %d)",
+				backup.BackupID, backup.MaxCASCounter, maxCASCounter)
+		}
+		if maxBackupID < backup.BackupID {
+			// I suppose this conditional is always hit because the backups are in sorted order.
+			maxBackupID = backup.BackupID
+		}
+	}
+
+	f, err := ioutil.TempFile(path, "partial-backup-")
+	if err != nil {
+		return err
+	}
+	tmpName := f.Name()
 	defer func() {
 		if closeErr := f.Close(); err == nil {
 			err = closeErr
 		}
 	}()
 
-	return nil
+	for items := range itemCh {
+		if items == nil {
+			break
+		}
+		for _, item := range items {
+			data, err := item.Marshal()
+			if err != nil {
+				return err
+			}
+			if _, err := f.Write(data); err != nil {
+				return err
+			}
+		}
+	}
+
+	if err := f.Sync(); err != nil {
+		return err
+	}
+	err = f.Close()
+	f = nil
+	if err != nil {
+		return err
+	}
+
+	newBackupID := maxBackupID + 1
+	if err := os.Rename(filepath.Join(path, tmpName),
+		filepath.Join(path, backupFileName(newBackupID))); err != nil {
+		return err
+	}
+
+	// TODO: Sync dir?  Yadda yadda.
+
+	status.Backups = append(status.Backups, &protos.BackupStatusItem{
+		BackupID:      newBackupID,
+		MaxCASCounter: maxCASCounter,
+	})
+
+	return writeBackupStatus(path, status)
 }
--- protos/backup.pb.go
+++ protos/backup.pb.go
@@ -10,6 +10,8 @@
 
 	It has these top-level messages:
 		BackupItem
+		BackupStatus
+		BackupStatusItem
 		ManifestChangeSet
 		ManifestChange
 */
@@ -74,8 +76,54 @@ func (m *BackupItem) GetValue() []byte {
 	return nil
 }
 
+// BackupStatus describes the state of a backup store -- what backups are made, their max cas
+// values, other info
+type BackupStatus struct {
+	Backups []*BackupStatusItem `protobuf:"bytes,1,rep,name=Backups" json:"Backups,omitempty"`
+}
+
+func (m *BackupStatus) Reset()                    { *m = BackupStatus{} }
+func (m *BackupStatus) String() string            { return proto.CompactTextString(m) }
+func (*BackupStatus) ProtoMessage()               {}
+func (*BackupStatus) Descriptor() ([]byte, []int) { return fileDescriptorBackup, []int{1} }
+
+func (m *BackupStatus) GetBackups() []*BackupStatusItem {
+	if m != nil {
+		return m.Backups
+	}
+	return nil
+}
+
+// BackupStatusItem describes a single backup.  (It might identify the original backup or an
+// incremental backup after that.)
+type BackupStatusItem struct {
+	BackupID      uint64 `protobuf:"varint,1,opt,name=BackupID,proto3" json:"BackupID,omitempty"`
+	MaxCASCounter uint64 `protobuf:"varint,2,opt,name=MaxCASCounter,proto3" json:"MaxCASCounter,omitempty"`
+}
+
+func (m *BackupStatusItem) Reset()                    { *m = BackupStatusItem{} }
+func (m *BackupStatusItem) String() string            { return proto.CompactTextString(m) }
+func (*BackupStatusItem) ProtoMessage()               {}
+func (*BackupStatusItem) Descriptor() ([]byte, []int) { return fileDescriptorBackup, []int{2} }
+
+func (m *BackupStatusItem) GetBackupID() uint64 {
+	if m != nil {
+		return m.BackupID
+	}
+	return 0
+}
+
+func (m *BackupStatusItem) GetMaxCASCounter() uint64 {
+	if m != nil {
+		return m.MaxCASCounter
+	}
+	return 0
+}
+
 func init() {
 	proto.RegisterType((*BackupItem)(nil), "protos.BackupItem")
+	proto.RegisterType((*BackupStatus)(nil), "protos.BackupStatus")
+	proto.RegisterType((*BackupStatusItem)(nil), "protos.BackupStatusItem")
 }
 func (m *BackupItem) Marshal() (dAtA []byte, err error) {
 	size := m.Size()
@@ -118,6 +166,64 @@ func (m *BackupItem) MarshalTo(dAtA []byte) (int, error) {
 		i++
 		i = encodeVarintBackup(dAtA, i, uint64(len(m.Value)))
 		i += copy(dAtA[i:], m.Value)
+	}
+	return i, nil
+}
+
+func (m *BackupStatus) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalTo(dAtA)
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *BackupStatus) MarshalTo(dAtA []byte) (int, error) {
+	var i int
+	_ = i
+	var l int
+	_ = l
+	if len(m.Backups) > 0 {
+		for _, msg := range m.Backups {
+			dAtA[i] = 0xa
+			i++
+			i = encodeVarintBackup(dAtA, i, uint64(msg.Size()))
+			n, err := msg.MarshalTo(dAtA[i:])
+			if err != nil {
+				return 0, err
+			}
+			i += n
+		}
+	}
+	return i, nil
+}
+
+func (m *BackupStatusItem) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalTo(dAtA)
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *BackupStatusItem) MarshalTo(dAtA []byte) (int, error) {
+	var i int
+	_ = i
+	var l int
+	_ = l
+	if m.BackupID != 0 {
+		dAtA[i] = 0x8
+		i++
+		i = encodeVarintBackup(dAtA, i, uint64(m.BackupID))
+	}
+	if m.MaxCASCounter != 0 {
+		dAtA[i] = 0x10
+		i++
+		i = encodeVarintBackup(dAtA, i, uint64(m.MaxCASCounter))
 	}
 	return i, nil
 }
@@ -165,6 +271,30 @@ func (m *BackupItem) Size() (n int) {
 	l = len(m.Value)
 	if l > 0 {
 		n += 1 + l + sovBackup(uint64(l))
+	}
+	return n
+}
+
+func (m *BackupStatus) Size() (n int) {
+	var l int
+	_ = l
+	if len(m.Backups) > 0 {
+		for _, e := range m.Backups {
+			l = e.Size()
+			n += 1 + l + sovBackup(uint64(l))
+		}
+	}
+	return n
+}
+
+func (m *BackupStatusItem) Size() (n int) {
+	var l int
+	_ = l
+	if m.BackupID != 0 {
+		n += 1 + sovBackup(uint64(m.BackupID))
+	}
+	if m.MaxCASCounter != 0 {
+		n += 1 + sovBackup(uint64(m.MaxCASCounter))
 	}
 	return n
 }
@@ -333,6 +463,175 @@ func (m *BackupItem) Unmarshal(dAtA []byte) error {
 	}
 	return nil
 }
+func (m *BackupStatus) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowBackup
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= (uint64(b) & 0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: BackupStatus: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: BackupStatus: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Backups", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowBackup
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= (int(b) & 0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthBackup
+			}
+			postIndex := iNdEx + msglen
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Backups = append(m.Backups, &BackupStatusItem{})
+			if err := m.Backups[len(m.Backups)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipBackup(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if skippy < 0 {
+				return ErrInvalidLengthBackup
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *BackupStatusItem) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowBackup
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= (uint64(b) & 0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: BackupStatusItem: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: BackupStatusItem: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field BackupID", wireType)
+			}
+			m.BackupID = 0
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowBackup
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				m.BackupID |= (uint64(b) & 0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+		case 2:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field MaxCASCounter", wireType)
+			}
+			m.MaxCASCounter = 0
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowBackup
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				m.MaxCASCounter |= (uint64(b) & 0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+		default:
+			iNdEx = preIndex
+			skippy, err := skipBackup(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if skippy < 0 {
+				return ErrInvalidLengthBackup
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
 func skipBackup(dAtA []byte) (n int, err error) {
 	l := len(dAtA)
 	iNdEx := 0
@@ -441,15 +740,19 @@ var (
 func init() { proto.RegisterFile("backup.proto", fileDescriptorBackup) }
 
 var fileDescriptorBackup = []byte{
-	// 147 bytes of a gzipped FileDescriptorProto
+	// 215 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x49, 0x4a, 0x4c, 0xce,
 	0x2e, 0x2d, 0xd0, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x03, 0x53, 0xc5, 0x4a, 0x05, 0x5c,
 	0x5c, 0x4e, 0x60, 0x71, 0xcf, 0x92, 0xd4, 0x5c, 0x21, 0x01, 0x2e, 0x66, 0xef, 0xd4, 0x4a, 0x09,
 	0x46, 0x05, 0x46, 0x0d, 0x9e, 0x20, 0x10, 0x53, 0x48, 0x8e, 0x8b, 0xcb, 0xd9, 0x31, 0xd8, 0x39,
 	0xbf, 0x34, 0xaf, 0x24, 0xb5, 0x48, 0x82, 0x49, 0x81, 0x51, 0x83, 0x25, 0x08, 0x49, 0x44, 0x48,
 	0x8a, 0x8b, 0xc3, 0x23, 0xb1, 0x38, 0x2c, 0x31, 0xa7, 0x34, 0x55, 0x82, 0x59, 0x81, 0x51, 0x83,
-	0x23, 0x08, 0xce, 0x17, 0x12, 0xe1, 0x62, 0x85, 0x48, 0xb0, 0x80, 0xcd, 0x83, 0x70, 0x9c, 0x04,
-	0x4e, 0x3c, 0x92, 0x63, 0xbc, 0xf0, 0x48, 0x8e, 0xf1, 0xc1, 0x23, 0x39, 0xc6, 0x19, 0x8f, 0xe5,
-	0x18, 0x92, 0x20, 0x6e, 0x31, 0x06, 0x04, 0x00, 0x00, 0xff, 0xff, 0x6d, 0x0f, 0x59, 0xd7, 0xa2,
-	0x00, 0x00, 0x00,
+	0x23, 0x08, 0xce, 0x17, 0x12, 0xe1, 0x62, 0x85, 0x48, 0xb0, 0x80, 0xcd, 0x83, 0x70, 0x94, 0x9c,
+	0xb8, 0x78, 0x20, 0x36, 0x06, 0x97, 0x24, 0x96, 0x94, 0x16, 0x0b, 0x19, 0x71, 0xb1, 0x43, 0xf8,
+	0xc5, 0x12, 0x8c, 0x0a, 0xcc, 0x1a, 0xdc, 0x46, 0x12, 0x10, 0x27, 0x16, 0xeb, 0x21, 0x2b, 0x03,
+	0x39, 0x2f, 0x08, 0xa6, 0x50, 0x29, 0x84, 0x4b, 0x00, 0x5d, 0x12, 0xe4, 0x12, 0xa8, 0x4f, 0x5c,
+	0xc0, 0x1e, 0x60, 0x09, 0x82, 0xf3, 0x85, 0x54, 0xb8, 0x78, 0x7d, 0x13, 0x2b, 0x30, 0x3c, 0x82,
+	0x2a, 0xe8, 0x24, 0x70, 0xe2, 0x91, 0x1c, 0xe3, 0x85, 0x47, 0x72, 0x8c, 0x0f, 0x1e, 0xc9, 0x31,
+	0xce, 0x78, 0x2c, 0xc7, 0x90, 0x04, 0x09, 0x25, 0x63, 0x40, 0x00, 0x00, 0x00, 0xff, 0xff, 0x5f,
+	0x3a, 0xed, 0xb5, 0x3c, 0x01, 0x00, 0x00,
 }
