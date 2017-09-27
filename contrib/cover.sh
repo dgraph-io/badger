bde731cfd6d5b1ef99b9655d66fc7d32cfd50c41
Pawan Rawal <pawan@dgraph.io>
2017-09-27 16:50:49 +1000

    Remove race flag for now.

--- contrib/cover.sh
+++ contrib/cover.sh
@@ -13,7 +13,7 @@ pushd $SRC &> /dev/null
 # create coverage output
 echo 'mode: atomic' > $OUT
 for PKG in $(go list ./...|grep -v -E 'vendor'); do
-  go test -race -covermode=atomic -coverprofile=$TMP $PKG
+  go test -covermode=atomic -coverprofile=$TMP $PKG
   tail -n +2 $TMP >> $OUT
 done
 
