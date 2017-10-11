d7726a5a55f8e58d4dedfe7c2fafc0c21f3ead45
Deepak Jois <deepak.jois@gmail.com>
2017-10-11 11:20:15 +0530

    Add method to delete older versions of keys.
    This method traverses the store and deletes any older versions of the
    keys. Deletes happen in batches of 1000 to avoid any memory overruns.

--- db.go
+++ db.go
@@ -17,6 +17,7 @@
 package badger
 
 import (
+	"bytes"
 	"container/heap"
 	"expvar"
 	"log"
@@ -862,6 +863,86 @@ func (db *DB) updateSize(lc *y.Closer) {
 			return
 		}
 	}
+}
+
+// PurgeOlderVersions deletes older versions of all keys.
+//
+// This function could be called prior to doing garbage collection to clean up
+// older versions that are no longer needed. The caller must make sure that
+// there are no long-running read transactions running before this function is
+// called, otherwise they will not work as expected.
+func (db *DB) PurgeOlderVersions() error {
+	return db.View(func(txn *Txn) error {
+		opts := DefaultIteratorOptions
+		opts.AllVersions = true
+		opts.PrefetchValues = false
+		it := txn.NewIterator(opts)
+
+		var entries []*entry
+		var lastKey []byte
+		var count int
+		var wg sync.WaitGroup
+		errChan := make(chan error, 1)
+
+		// func to check for pending error before sending off a batch for writing
+		batchSetAsyncIfNoErr := func(entries []*entry) error {
+			select {
+			case err := <-errChan:
+				return err
+			default:
+				wg.Add(1)
+				return txn.db.batchSetAsync(entries, func(err error) {
+					defer wg.Done()
+					if err != nil {
+						select {
+						case errChan <- err:
+						default:
+						}
+					}
+				})
+			}
+		}
+
+		for it.Rewind(); it.Valid(); it.Next() {
+			item := it.Item()
+			if !bytes.Equal(lastKey, item.Key()) {
+				lastKey = y.Safecopy(lastKey, item.Key())
+				continue
+			}
+			// Found an older version. Mark for deletion
+			entries = append(entries,
+				&entry{
+					Key:  y.KeyWithTs(lastKey, item.version),
+					Meta: bitDelete,
+				})
+			count++
+
+			// Batch up 1000 entries at a time and write
+			if count == 1000 {
+				if err := batchSetAsyncIfNoErr(entries); err != nil {
+					return err
+				}
+				count = 0
+				entries = []*entry{}
+			}
+		}
+
+		// Write last batch pending deletes
+		if count > 0 {
+			if err := batchSetAsyncIfNoErr(entries); err != nil {
+				return err
+			}
+		}
+
+		wg.Wait()
+
+		select {
+		case err := <-errChan:
+			return err
+		default:
+			return nil
+		}
+	})
 }
 
 // RunValueLogGC would trigger a value log garbage collection with no guarantees that a call would
