5aaa4b691e47ec676420291fe776204e8f395786
Deepak Jois <deepak.jois@gmail.com>
2017-10-26 17:04:12 +0530

    Add CLI for Badger backup and restore.
    * `badger backup` creates a backup of the database
    
    * `badger restore` creates a database and restores it from a file
    created by the command above. It can also restore backups created using
    another version of Badger.
    
      At the moment, `badger restore` does not support restoring to existing
      databases. This is because we do not want the user to inadvertently get
      into an inconsistent state due to version mismatches. We will re-enable
      this once we have support for incremental restores, by making sure the
      user sets a flag to indicate their intention to do incremental restore.

---
+++ cmd/badger/cmd/root.go
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2017 Dgraph Labs, Inc. and Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var sstDir, vlogDir string
+
+// RootCmd represents the base command when called without any subcommands
+var RootCmd = &cobra.Command{
+	Use:               "badger",
+	Short:             "Tool to manage Badger databases.",
+	PersistentPreRunE: validateRootCmdArgs,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	RootCmd.PersistentFlags().StringVar(&sstDir, "dir", "",
+		"Directory where the LSM tree files are located. (required)")
+
+	RootCmd.PersistentFlags().StringVar(&vlogDir, "vlog-dir", "",
+		"Directory where the value log files are located, if different from --sst-dir")
+}
+
+func validateRootCmdArgs(cmd *cobra.Command, args []string) error {
+	if strings.HasPrefix(cmd.Use, "help ") { // No need to validate if it is help
+		return nil
+	}
+	if sstDir == "" {
+		return errors.New("--sst-dir not specified")
+	}
+
+	if vlogDir == "" {
+		vlogDir = sstDir
+	}
+	return nil
+}
