f9eeac3a630cbf176320d307d648341a44f38fd6
Deepak Jois <deepak.jois@gmail.com>
2017-12-11 11:08:42 +0530

    Fix some warnings in code added while implementing DB.GetSequence()

--- managed_db.go
+++ managed_db.go
@@ -79,7 +79,8 @@ func (db *ManagedDB) PurgeVersionsBelow(key []byte, ts uint64) error {
 	return db.purgeVersionsBelow(txn, key, ts)
 }
 
+// GetSequence is not supported on ManagedDB. Calling this would result
+// in a panic.
 func (db *ManagedDB) GetSequence(_ []byte, _ uint64) (*Sequence, error) {
 	panic("Cannot use GetSequence for ManagedDB.")
-	return nil, ErrInvalidRequest
 }
