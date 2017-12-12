3c3a3ac1996b39444573955c1cadb7fb756fb3f7
Deepak Jois <deepak.jois@gmail.com>
2017-12-12 12:46:29 +0530

    Store TTL in nanoseconds
    This involves a change in the way Badger stores data on disk, so
    we need to increment the magic version as well.

--- manifest.go
+++ manifest.go
@@ -211,7 +211,7 @@ func (mf *manifestFile) addChanges(changesParam []*protos.ManifestChange) error 
 var magicText = [4]byte{'B', 'd', 'g', 'r'}
 
 // The magic version number.
-const magicVersion = 4
+const magicVersion = 5
 
 func helpRewrite(dir string, m *Manifest) (*os.File, int, error) {
 	rewritePath := filepath.Join(dir, manifestRewriteFilename)
