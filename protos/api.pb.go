0995f11508736b1277802918545cbf46f55f284c
Janardhan Reddy <janardhan@dgraph.io>
2018-01-02 14:26:40 +1100

    Add ValueLogIndex proto

--- protos/api.pb.go
+++ protos/api.pb.go
@@ -11,6 +11,8 @@
 		ManifestChangeSet
 		ManifestChange
 		KVPair
+		ValueLogIndex
+		IdOffset
 */
 package protos
 
@@ -151,10 +153,61 @@ func (m *KVPair) GetExpiresAt() uint64 {
 	return 0
 }
 
+type ValueLogIndex struct {
+	// Only one of the following would be set depending on index sparsness
+	Offsets   []uint32    `protobuf:"varint,1,rep,packed,name=offsets" json:"offsets,omitempty"`
+	IdOffsets []*IdOffset `protobuf:"bytes,2,rep,name=id_offsets,json=idOffsets" json:"id_offsets,omitempty"`
+}
+
+func (m *ValueLogIndex) Reset()                    { *m = ValueLogIndex{} }
+func (m *ValueLogIndex) String() string            { return proto.CompactTextString(m) }
+func (*ValueLogIndex) ProtoMessage()               {}
+func (*ValueLogIndex) Descriptor() ([]byte, []int) { return fileDescriptorApi, []int{3} }
+
+func (m *ValueLogIndex) GetOffsets() []uint32 {
+	if m != nil {
+		return m.Offsets
+	}
+	return nil
+}
+
+func (m *ValueLogIndex) GetIdOffsets() []*IdOffset {
+	if m != nil {
+		return m.IdOffsets
+	}
+	return nil
+}
+
+type IdOffset struct {
+	Id     uint32 `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
+	Offset uint32 `protobuf:"varint,2,opt,name=offset,proto3" json:"offset,omitempty"`
+}
+
+func (m *IdOffset) Reset()                    { *m = IdOffset{} }
+func (m *IdOffset) String() string            { return proto.CompactTextString(m) }
+func (*IdOffset) ProtoMessage()               {}
+func (*IdOffset) Descriptor() ([]byte, []int) { return fileDescriptorApi, []int{4} }
+
+func (m *IdOffset) GetId() uint32 {
+	if m != nil {
+		return m.Id
+	}
+	return 0
+}
+
+func (m *IdOffset) GetOffset() uint32 {
+	if m != nil {
+		return m.Offset
+	}
+	return 0
+}
+
 func init() {
 	proto.RegisterType((*ManifestChangeSet)(nil), "protos.ManifestChangeSet")
 	proto.RegisterType((*ManifestChange)(nil), "protos.ManifestChange")
 	proto.RegisterType((*KVPair)(nil), "protos.KVPair")
+	proto.RegisterType((*ValueLogIndex)(nil), "protos.ValueLogIndex")
+	proto.RegisterType((*IdOffset)(nil), "protos.IdOffset")
 	proto.RegisterEnum("protos.ManifestChange_Operation", ManifestChange_Operation_name, ManifestChange_Operation_value)
 }
 func (m *ManifestChangeSet) Marshal() (dAtA []byte, err error) {
@@ -266,6 +319,81 @@ func (m *KVPair) MarshalTo(dAtA []byte) (int, error) {
 	return i, nil
 }
 
+func (m *ValueLogIndex) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalTo(dAtA)
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *ValueLogIndex) MarshalTo(dAtA []byte) (int, error) {
+	var i int
+	_ = i
+	var l int
+	_ = l
+	if len(m.Offsets) > 0 {
+		dAtA2 := make([]byte, len(m.Offsets)*10)
+		var j1 int
+		for _, num := range m.Offsets {
+			for num >= 1<<7 {
+				dAtA2[j1] = uint8(uint64(num)&0x7f | 0x80)
+				num >>= 7
+				j1++
+			}
+			dAtA2[j1] = uint8(num)
+			j1++
+		}
+		dAtA[i] = 0xa
+		i++
+		i = encodeVarintApi(dAtA, i, uint64(j1))
+		i += copy(dAtA[i:], dAtA2[:j1])
+	}
+	if len(m.IdOffsets) > 0 {
+		for _, msg := range m.IdOffsets {
+			dAtA[i] = 0x12
+			i++
+			i = encodeVarintApi(dAtA, i, uint64(msg.Size()))
+			n, err := msg.MarshalTo(dAtA[i:])
+			if err != nil {
+				return 0, err
+			}
+			i += n
+		}
+	}
+	return i, nil
+}
+
+func (m *IdOffset) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalTo(dAtA)
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *IdOffset) MarshalTo(dAtA []byte) (int, error) {
+	var i int
+	_ = i
+	var l int
+	_ = l
+	if m.Id != 0 {
+		dAtA[i] = 0x8
+		i++
+		i = encodeVarintApi(dAtA, i, uint64(m.Id))
+	}
+	if m.Offset != 0 {
+		dAtA[i] = 0x10
+		i++
+		i = encodeVarintApi(dAtA, i, uint64(m.Offset))
+	}
+	return i, nil
+}
+
 func encodeFixed64Api(dAtA []byte, offset int, v uint64) int {
 	dAtA[offset] = uint8(v)
 	dAtA[offset+1] = uint8(v >> 8)
@@ -340,6 +468,37 @@ func (m *KVPair) Size() (n int) {
 	}
 	if m.ExpiresAt != 0 {
 		n += 1 + sovApi(uint64(m.ExpiresAt))
+	}
+	return n
+}
+
+func (m *ValueLogIndex) Size() (n int) {
+	var l int
+	_ = l
+	if len(m.Offsets) > 0 {
+		l = 0
+		for _, e := range m.Offsets {
+			l += sovApi(uint64(e))
+		}
+		n += 1 + sovApi(uint64(l)) + l
+	}
+	if len(m.IdOffsets) > 0 {
+		for _, e := range m.IdOffsets {
+			l = e.Size()
+			n += 1 + l + sovApi(uint64(l))
+		}
+	}
+	return n
+}
+
+func (m *IdOffset) Size() (n int) {
+	var l int
+	_ = l
+	if m.Id != 0 {
+		n += 1 + sovApi(uint64(m.Id))
+	}
+	if m.Offset != 0 {
+		n += 1 + sovApi(uint64(m.Offset))
 	}
 	return n
 }
@@ -726,6 +885,237 @@ func (m *KVPair) Unmarshal(dAtA []byte) error {
 	}
 	return nil
 }
+func (m *ValueLogIndex) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowApi
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= (uint64(b) & 0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: ValueLogIndex: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: ValueLogIndex: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType == 0 {
+				var v uint32
+				for shift := uint(0); ; shift += 7 {
+					if shift >= 64 {
+						return ErrIntOverflowApi
+					}
+					if iNdEx >= l {
+						return io.ErrUnexpectedEOF
+					}
+					b := dAtA[iNdEx]
+					iNdEx++
+					v |= (uint32(b) & 0x7F) << shift
+					if b < 0x80 {
+						break
+					}
+				}
+				m.Offsets = append(m.Offsets, v)
+			} else if wireType == 2 {
+				var packedLen int
+				for shift := uint(0); ; shift += 7 {
+					if shift >= 64 {
+						return ErrIntOverflowApi
+					}
+					if iNdEx >= l {
+						return io.ErrUnexpectedEOF
+					}
+					b := dAtA[iNdEx]
+					iNdEx++
+					packedLen |= (int(b) & 0x7F) << shift
+					if b < 0x80 {
+						break
+					}
+				}
+				if packedLen < 0 {
+					return ErrInvalidLengthApi
+				}
+				postIndex := iNdEx + packedLen
+				if postIndex > l {
+					return io.ErrUnexpectedEOF
+				}
+				for iNdEx < postIndex {
+					var v uint32
+					for shift := uint(0); ; shift += 7 {
+						if shift >= 64 {
+							return ErrIntOverflowApi
+						}
+						if iNdEx >= l {
+							return io.ErrUnexpectedEOF
+						}
+						b := dAtA[iNdEx]
+						iNdEx++
+						v |= (uint32(b) & 0x7F) << shift
+						if b < 0x80 {
+							break
+						}
+					}
+					m.Offsets = append(m.Offsets, v)
+				}
+			} else {
+				return fmt.Errorf("proto: wrong wireType = %d for field Offsets", wireType)
+			}
+		case 2:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field IdOffsets", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowApi
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= (int(b) & 0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthApi
+			}
+			postIndex := iNdEx + msglen
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.IdOffsets = append(m.IdOffsets, &IdOffset{})
+			if err := m.IdOffsets[len(m.IdOffsets)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipApi(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if skippy < 0 {
+				return ErrInvalidLengthApi
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *IdOffset) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowApi
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= (uint64(b) & 0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: IdOffset: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: IdOffset: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Id", wireType)
+			}
+			m.Id = 0
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowApi
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				m.Id |= (uint32(b) & 0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+		case 2:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Offset", wireType)
+			}
+			m.Offset = 0
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowApi
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				m.Offset |= (uint32(b) & 0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+		default:
+			iNdEx = preIndex
+			skippy, err := skipApi(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if skippy < 0 {
+				return ErrInvalidLengthApi
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
 func skipApi(dAtA []byte) (n int, err error) {
 	l := len(dAtA)
 	iNdEx := 0
@@ -834,24 +1224,29 @@ var (
 func init() { proto.RegisterFile("api.proto", fileDescriptorApi) }
 
 var fileDescriptorApi = []byte{
-	// 300 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x6c, 0x90, 0xcf, 0x4a, 0x33, 0x31,
-	0x14, 0xc5, 0x9b, 0xe9, 0xbf, 0xaf, 0xf7, 0xab, 0x65, 0x0c, 0x22, 0x41, 0x70, 0x18, 0xc6, 0xcd,
-	0xac, 0x4a, 0xa9, 0x4f, 0x50, 0x6b, 0x16, 0xc5, 0x96, 0x4a, 0x2c, 0x6e, 0x25, 0xda, 0xab, 0x06,
-	0xcb, 0x4c, 0x48, 0xd2, 0xa2, 0x0f, 0xe0, 0xd2, 0xbd, 0x8f, 0xe4, 0xd2, 0x47, 0x90, 0xfa, 0x22,
-	0x32, 0x19, 0x5b, 0x28, 0xb8, 0xca, 0xfd, 0xe5, 0x9c, 0x9c, 0x9c, 0x04, 0x5a, 0x52, 0xab, 0xae,
-	0x36, 0xb9, 0xcb, 0x69, 0xc3, 0x2f, 0x36, 0xe1, 0xb0, 0x3f, 0x91, 0x99, 0xba, 0x47, 0xeb, 0x86,
-	0x8f, 0x32, 0x7b, 0xc0, 0x2b, 0x74, 0xb4, 0x07, 0xcd, 0x3b, 0x0f, 0x96, 0x91, 0xb8, 0x9a, 0xfe,
-	0xef, 0x1f, 0x96, 0xa7, 0x6c, 0x77, 0xd7, 0x2b, 0x36, 0xb6, 0xe4, 0x8d, 0x40, 0x67, 0x57, 0xa3,
-	0x1d, 0x08, 0x46, 0x73, 0x46, 0x62, 0x92, 0xd6, 0x44, 0x30, 0x9a, 0xd3, 0x1e, 0x04, 0x53, 0xcd,
-	0x82, 0x98, 0xa4, 0x9d, 0x7e, 0xfc, 0x77, 0x5e, 0x77, 0xaa, 0xd1, 0x48, 0xa7, 0xf2, 0x4c, 0x04,
-	0x53, 0x4d, 0x0f, 0xa0, 0x3e, 0xc6, 0x15, 0x2e, 0x58, 0x35, 0x26, 0xe9, 0x9e, 0x28, 0x21, 0x39,
-	0x81, 0xd6, 0xd6, 0x46, 0x01, 0x1a, 0x43, 0xc1, 0x07, 0x33, 0x1e, 0x56, 0x8a, 0xf9, 0x9c, 0x8f,
-	0xf9, 0x8c, 0x87, 0x24, 0x79, 0x25, 0xd0, 0xb8, 0xb8, 0xbe, 0x94, 0xca, 0xd0, 0x10, 0xaa, 0x4f,
-	0xf8, 0xe2, 0x8b, 0xb4, 0x45, 0x31, 0x16, 0xb9, 0x2b, 0xb9, 0x58, 0xa2, 0x2f, 0xd3, 0x16, 0x25,
-	0xd0, 0x23, 0xf8, 0xb7, 0xb4, 0x68, 0x26, 0xe8, 0xa4, 0xbf, 0xb0, 0x2d, 0xb6, 0x4c, 0x19, 0x34,
-	0x57, 0x68, 0xac, 0xca, 0x33, 0x56, 0xf3, 0x0f, 0xda, 0x20, 0x3d, 0x06, 0xc0, 0x67, 0xad, 0x0c,
-	0xda, 0x1b, 0xe9, 0x58, 0xdd, 0x8b, 0xad, 0xdf, 0x9d, 0x81, 0x3b, 0x0b, 0x3f, 0xd6, 0x11, 0xf9,
-	0x5c, 0x47, 0xe4, 0x6b, 0x1d, 0x91, 0xf7, 0xef, 0xa8, 0x72, 0x5b, 0x7e, 0xfc, 0xe9, 0x4f, 0x00,
-	0x00, 0x00, 0xff, 0xff, 0x99, 0x84, 0xe3, 0x99, 0x8c, 0x01, 0x00, 0x00,
+	// 370 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x6c, 0x92, 0xc1, 0x6a, 0xea, 0x40,
+	0x14, 0x86, 0x9d, 0xa8, 0xd1, 0x9c, 0x6b, 0x24, 0x77, 0xb8, 0x48, 0xb8, 0xd0, 0x10, 0xd2, 0x4d,
+	0x56, 0x56, 0xec, 0x13, 0x58, 0x9b, 0x45, 0xa8, 0x92, 0x32, 0x15, 0x17, 0xdd, 0xc8, 0xb4, 0x19,
+	0xed, 0x50, 0x49, 0x42, 0x66, 0x14, 0xfb, 0x00, 0x5d, 0x76, 0xdf, 0x47, 0xea, 0xb2, 0x8f, 0x50,
+	0xec, 0x8b, 0x94, 0x4c, 0x8c, 0x20, 0x74, 0x95, 0xf3, 0x9d, 0xff, 0xe4, 0xcf, 0x39, 0x3f, 0x01,
+	0x83, 0x66, 0xbc, 0x9f, 0xe5, 0xa9, 0x4c, 0xb1, 0xae, 0x1e, 0xc2, 0x0b, 0xe0, 0xef, 0x94, 0x26,
+	0x7c, 0xc9, 0x84, 0x1c, 0x3f, 0xd1, 0x64, 0xc5, 0xee, 0x98, 0xc4, 0x03, 0x68, 0x3d, 0x2a, 0x10,
+	0x36, 0x72, 0xeb, 0xfe, 0x9f, 0x61, 0xaf, 0x7c, 0x4b, 0xf4, 0x4f, 0x67, 0x49, 0x35, 0xe6, 0xbd,
+	0x21, 0xe8, 0x9e, 0x6a, 0xb8, 0x0b, 0x5a, 0x18, 0xdb, 0xc8, 0x45, 0x7e, 0x83, 0x68, 0x61, 0x8c,
+	0x07, 0xa0, 0x45, 0x99, 0xad, 0xb9, 0xc8, 0xef, 0x0e, 0xdd, 0xdf, 0xfd, 0xfa, 0x51, 0xc6, 0x72,
+	0x2a, 0x79, 0x9a, 0x10, 0x2d, 0xca, 0xf0, 0x3f, 0x68, 0x4e, 0xd8, 0x96, 0xad, 0xed, 0xba, 0x8b,
+	0x7c, 0x93, 0x94, 0xe0, 0x9d, 0x83, 0x71, 0x1c, 0xc3, 0x00, 0xfa, 0x98, 0x04, 0xa3, 0x59, 0x60,
+	0xd5, 0x8a, 0xfa, 0x3a, 0x98, 0x04, 0xb3, 0xc0, 0x42, 0xde, 0x2b, 0x02, 0xfd, 0x66, 0x7e, 0x4b,
+	0x79, 0x8e, 0x2d, 0xa8, 0x3f, 0xb3, 0x17, 0xb5, 0x48, 0x87, 0x14, 0x65, 0xe1, 0xbb, 0xa5, 0xeb,
+	0x0d, 0x53, 0xcb, 0x74, 0x48, 0x09, 0xf8, 0x3f, 0xb4, 0x37, 0x82, 0xe5, 0x53, 0x26, 0xa9, 0xfa,
+	0x60, 0x87, 0x1c, 0x19, 0xdb, 0xd0, 0xda, 0xb2, 0x5c, 0xf0, 0x34, 0xb1, 0x1b, 0xea, 0xa0, 0x0a,
+	0xf1, 0x19, 0x00, 0xdb, 0x65, 0x3c, 0x67, 0x62, 0x41, 0xa5, 0xdd, 0x54, 0xa2, 0x71, 0xe8, 0x8c,
+	0xa4, 0x77, 0x0f, 0xe6, 0xbc, 0x70, 0x9f, 0xa4, 0xab, 0x30, 0x89, 0xd9, 0xae, 0x70, 0x4a, 0x97,
+	0x4b, 0xc1, 0x64, 0x19, 0xad, 0x49, 0x2a, 0xc4, 0x17, 0x00, 0x3c, 0x5e, 0x54, 0xa2, 0xa6, 0x72,
+	0xb7, 0xaa, 0x9c, 0xc2, 0x38, 0x52, 0x02, 0x31, 0xf8, 0xa1, 0x12, 0xde, 0x10, 0xda, 0x55, 0xbb,
+	0x08, 0x9b, 0x97, 0x61, 0x9b, 0x44, 0xe3, 0x31, 0xee, 0x81, 0x5e, 0x3a, 0xa9, 0x1b, 0x4d, 0x72,
+	0xa0, 0x2b, 0xeb, 0x63, 0xef, 0xa0, 0xcf, 0xbd, 0x83, 0xbe, 0xf6, 0x0e, 0x7a, 0xff, 0x76, 0x6a,
+	0x0f, 0xe5, 0x8f, 0x70, 0xf9, 0x13, 0x00, 0x00, 0xff, 0xff, 0xc4, 0x0c, 0x71, 0x6e, 0x1c, 0x02,
+	0x00, 0x00,
 }
