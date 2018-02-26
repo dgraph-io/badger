26750f3570a4049d18bf1fd9ab2ec92a8804f686
Hilko Bengen <bengen--github@hilluzination.de>
2018-02-26 12:04:01 +0100

    Commit: early callback (#413)
    * Add a test for deadlock
    
    * On commit, run unlock callbacks before starting writes.
    
    This should make sure that all read locks that may block starting a
    new value log file are released. (#410)

--- db_test.go
+++ db_test.go
@@ -1212,6 +1212,54 @@ func TestCreateDirs(t *testing.T) {
 	_, err = os.Stat(dir)
 	require.NoError(t, err)
 }
+
+func TestGetSetDeadlock(t *testing.T) {
+	dir, err := ioutil.TempDir("", "badger")
+	fmt.Println(dir)
+	require.NoError(t, err)
+	defer os.RemoveAll(dir)
+
+	opt := DefaultOptions
+	opt.Dir = dir
+	opt.ValueDir = dir
+	opt.ValueLogFileSize = 1 << 20
+	db, err := Open(opt)
+	require.NoError(t, err)
+	defer db.Close()
+
+	val := make([]byte, 1<<19)
+	key := []byte("key1")
+	require.NoError(t, db.Update(func(txn *Txn) error {
+		rand.Read(val)
+		require.NoError(t, txn.Set(key, val))
+		return nil
+	}))
+
+	timeout, done := time.After(10*time.Second), make(chan bool)
+
+	go func() {
+		db.Update(func(txn *Txn) error {
+			item, err := txn.Get(key)
+			require.NoError(t, err)
+			_, err = item.Value() // This take a RLock on file
+			require.NoError(t, err)
+
+			rand.Read(val)
+			require.NoError(t, txn.Set(key, val))
+			require.NoError(t, txn.Set([]byte("key2"), val))
+			return nil
+		})
+		done <- true
+	}()
+
+	select {
+	case <-timeout:
+		t.Fatal("db.Update did not finish within 10s, assuming deadlock.")
+	case <-done:
+		t.Log("db.Update finished.")
+	}
+}
+
 func TestWriteDeadlock(t *testing.T) {
 	dir, err := ioutil.TempDir("", "badger")
 	fmt.Println(dir)
