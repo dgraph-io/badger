4751ef1180486b0a270988c35606ddbb277eca89
Manish R Jain <manish@dgraph.io>
2018-11-25 13:11:43 -0800

    Key Splits
    Get rough key ranges from Badger to divide up iteration tasks over the
    DB.

--- iterator_test.go
+++ iterator_test.go
@@ -172,6 +172,8 @@ func BenchmarkIteratePrefixSingleKey(b *testing.B) {
 	})
 	y.Check(err)
 	b.Logf("LSM files: %d", lsmFiles)
+	b.Logf("Key splits: %v", db.KeySplits(nil))
+	b.Logf("Key splits with prefix: %v", db.KeySplits([]byte("09")))
 
 	b.Logf("Outer b.N: %d", b.N)
 	b.Run("Key lookups", func(b *testing.B) {
