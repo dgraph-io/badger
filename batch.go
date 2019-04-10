e0de0f86d09fc9b21311bb7faf048795b70208c0
ashish <ashish@dgraph.io>
2019-04-10 23:15:45 +0530

    Throttle pending transactions in WriteBatch
    Currently there is no limit on number of pending transactions(
    uncomitted transactions) in WriteBatch. Sometimes this leads to
    huge memory consumptions and atlast crashing of Badger. This commit
    implements throttling in WriteBatch and hence limits pending
    transactions count to a threshold.

--- batch.go
+++ batch.go
@@ -18,16 +18,20 @@ package badger
 
 import (
 	"sync"
+	"sync/atomic"
 	"time"
+
+	"github.com/dgraph-io/badger/y"
 )
 
 // WriteBatch holds the necessary info to perform batched writes.
 type WriteBatch struct {
 	sync.Mutex
-	txn *Txn
-	db  *DB
-	wg  sync.WaitGroup
-	err error
+	txn      *Txn
+	db       *DB
+	throttle *y.Throttle
+	err      error
+	flushed  int32 // flushed is used to avoid calling throttle.Finish twice
 }
 
 // NewWriteBatch creates a new WriteBatch. This provides a way to conveniently do a lot of writes,
@@ -36,7 +40,18 @@ type WriteBatch struct {
 // creating and committing transactions. Due to the nature of SSI guaratees provided by Badger,
 // blind writes can never encounter transaction conflicts (ErrConflict).
 func (db *DB) NewWriteBatch() *WriteBatch {
-	return &WriteBatch{db: db, txn: db.newTransaction(true, true)}
+	return &WriteBatch{
+		db:       db,
+		txn:      db.newTransaction(true, true),
+		throttle: y.NewThrottle(16),
+	}
+}
+
+// SetMaxPendingTxns sets a limit on maximum number of pending transactions while writing batches.
+// This function should be called before using WriteBatch. Default value of MaxPendingTxns is
+// 16 to minimise memory usage.
+func (wb *WriteBatch) SetMaxPendingTxns(max int) {
+	wb.throttle = y.NewThrottle(max)
 }
 
 // Cancel function must be called if there's a chance that Flush might not get
@@ -47,13 +62,21 @@ func (db *DB) NewWriteBatch() *WriteBatch {
 //
 // Note that any committed writes would still go through despite calling Cancel.
 func (wb *WriteBatch) Cancel() {
-	wb.wg.Wait()
+	// check if wb.throttle.Finsh() has been called already, if yes return.
+	if atomic.AddInt32(&wb.flushed, 1) > 1 {
+		atomic.AddInt32(&wb.flushed, -1)
+		return
+	}
+
+	if err := wb.throttle.Finish(); err != nil {
+		wb.db.opt.Errorf("WatchBatch.Cancel error while finishing: %v", err)
+	}
 	wb.txn.Discard()
 }
 
 func (wb *WriteBatch) callback(err error) {
 	// sync.WaitGroup is thread-safe, so it doesn't need to be run inside wb.Lock.
-	defer wb.wg.Done()
+	defer wb.throttle.Done(err)
 	if err == nil {
 		return
 	}
@@ -123,10 +146,10 @@ func (wb *WriteBatch) commit() error {
 	if wb.err != nil {
 		return wb.err
 	}
-	// Get a new txn before we commit this one. So, the new txn doesn't need
-	// to wait for this one to commit.
-	wb.wg.Add(1)
 	wb.txn.CommitWith(wb.callback)
+	if err := wb.throttle.Do(); err != nil {
+		return err
+	}
 	wb.txn = wb.db.newTransaction(true, true)
 	wb.txn.readTs = 0 // We're not reading anything.
 	return wb.err
@@ -140,8 +163,16 @@ func (wb *WriteBatch) Flush() error {
 	wb.txn.Discard()
 	wb.Unlock()
 
-	wb.wg.Wait()
-	// Safe to access error without any synchronization here.
+	// check if wb.throttle.Finish() has been already, if yes return.
+	if atomic.AddInt32(&wb.flushed, 1) > 1 {
+		atomic.AddInt32(&wb.flushed, -1)
+		return wb.err
+	}
+
+	if err := wb.throttle.Finish(); err != nil {
+		return err
+	}
+
 	return wb.err
 }
 
