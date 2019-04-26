763cedc39c746e9f1de6f3334b15c62920e92632
Manish R Jain <manish@dgraph.io>
2019-04-25 18:11:19 -0700

    Improve badger.fill by using default mmap options.

--- badger/cmd/fill.go
+++ badger/cmd/fill.go
@@ -21,7 +21,6 @@ import (
 	"time"
 
 	"github.com/dgraph-io/badger"
-	"github.com/dgraph-io/badger/options"
 	"github.com/dgraph-io/badger/y"
 	"github.com/spf13/cobra"
 )
@@ -57,8 +56,6 @@ func fill(cmd *cobra.Command, args []string) error {
 	opts.Truncate = truncate
 	opts.SyncWrites = false
 	opts.CompactL0OnClose = force
-	opts.TableLoadingMode = options.FileIO
-	opts.ValueLogLoadingMode = options.FileIO
 
 	db, err := badger.Open(opts)
 	if err != nil {
