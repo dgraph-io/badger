d8ab172c7ce0dd81f3bf75191c3a87f1f552c67a
பாலாஜி <rbalajis25@gmail.com>
2019-04-28 16:29:40 +0530

    - change publisher closer to pub - add nextID instead of lastID
    Signed-off-by: பாலாஜி <rbalajis25@gmail.com>

--- db.go
+++ db.go
@@ -54,7 +54,7 @@ type closers struct {
 	memtable   *y.Closer
 	writes     *y.Closer
 	valueGC    *y.Closer
-	publisher  *y.Closer
+	pub        *y.Closer
 }
 
 type callback func(kv *pb.KVList)
@@ -324,8 +324,8 @@ func Open(opt Options) (db *DB, err error) {
 	db.closers.valueGC = y.NewCloser(1)
 	go db.vlog.waitOnGC(db.closers.valueGC)
 
-	db.closers.publisher = y.NewCloser(1)
-	go db.pub.listenForUpdates(db.closers.publisher)
+	db.closers.pub = y.NewCloser(1)
+	go db.pub.listenForUpdates(db.closers.pub)
 
 	valueDirLockGuard = nil
 	dirLockGuard = nil
@@ -346,7 +346,7 @@ func (db *DB) Close() (err error) {
 	// Stop writes next.
 	db.closers.writes.SignalAndWait()
 
-	db.closers.publisher.SignalAndWait()
+	db.closers.pub.SignalAndWait()
 
 	// Now close the value log.
 	if vlogErr := db.vlog.Close(); vlogErr != nil {
@@ -1391,7 +1391,7 @@ func (db *DB) DropPrefix(prefix []byte) error {
 // Subscribe can be used watch key changes for the given key prefix
 func (db *DB) Subscribe(prefix []byte, cb callback) (func(), error) {
 	if cb == nil {
-		return nil, errors.New("callback can't be nil")
+		return nil, ErrNilCallback
 	}
 	c := y.NewCloser(1)
 	recvCh, id := db.pub.newSubscriber(prefix)
