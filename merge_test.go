8fce08a2cba70386cc0d043ced0adae639a5ac3a
Ibrahim Jarif <ibrahim@dgraph.io>
2019-04-30 15:34:56 +0530

    Move merge operator tests from db_test.go to merge_test.go file

--- merge_test.go
+++ merge_test.go
@@ -17,42 +17,116 @@
 package badger
 
 import (
+	"encoding/binary"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestGetMergeOperator(t *testing.T) {
+func TestGetMergeOperatorAddandGetSlices(t *testing.T) {
 	// Merge function to merge two byte slices
 	add := func(originalValue, newValue []byte) []byte {
 		// We append original value to new value because the values
 		// are retrieved in reverse order (Last insertion will be the first value)
 		return append(newValue, originalValue...)
 	}
-	t.Run("get should return error", func(t *testing.T) {
-		runBadgerTest(t, nil, func(t *testing.T, db *DB) {
-			m := db.GetMergeOperator([]byte("foo"), add, 200*time.Millisecond)
-			defer m.Stop()
+	runBadgerTest(t, nil, func(t *testing.T, db *DB) {
+		m := db.GetMergeOperator([]byte("fooprefix"), add, 2*time.Millisecond)
+		defer m.Stop()
 
-			value, err := m.Get()
-			// MergeOperator should return key not found error
-			require.Error(t, err)
-			require.Nil(t, value)
-		})
+		require.Nil(t, m.Add([]byte("1")))
+		require.Nil(t, m.Add([]byte("2")))
+		require.Nil(t, m.Add([]byte("3")))
+
+		value, err := m.Get()
+		require.Nil(t, err)
+		require.Equal(t, "123", string(value))
 	})
-	t.Run("add and get", func(t *testing.T) {
-		runBadgerTest(t, nil, func(t *testing.T, db *DB) {
-			m := db.GetMergeOperator([]byte("fooprefix"), add, 2*time.Millisecond)
-			defer m.Stop()
+}
 
-			require.Nil(t, m.Add([]byte("1")))
-			require.Nil(t, m.Add([]byte("2")))
-			require.Nil(t, m.Add([]byte("3")))
+func TestMergeOperatorGetBeforeAdd(t *testing.T) {
+	key := []byte("merge")
+	runBadgerTest(t, nil, func(t *testing.T, db *DB) {
+		m := db.GetMergeOperator(key, add, 200*time.Millisecond)
+		defer m.Stop()
 
-			value, err := m.Get()
-			require.Nil(t, err)
-			require.Equal(t, "123", string(value))
-		})
+		val, err := m.Get()
+		require.Equal(t, ErrKeyNotFound, err)
+		require.Nil(t, val)
 	})
+}
+
+func TestMergeOperatorAddAndGet(t *testing.T) {
+	key := []byte("merge")
+	runBadgerTest(t, nil, func(t *testing.T, db *DB) {
+		m := db.GetMergeOperator(key, add, 200*time.Millisecond)
+		defer m.Stop()
+
+		err := m.Add(uint64ToBytes(1))
+		require.NoError(t, err)
+		m.Add(uint64ToBytes(2))
+		require.NoError(t, err)
+		m.Add(uint64ToBytes(3))
+		require.NoError(t, err)
+
+		res, err := m.Get()
+		require.NoError(t, err)
+		require.Equal(t, uint64(6), bytesToUint64(res))
+	})
+}
+
+func TestMergeOperatorCompactBeforeGet(t *testing.T) {
+	key := []byte("merge")
+	runBadgerTest(t, nil, func(t *testing.T, db *DB) {
+		m := db.GetMergeOperator(key, add, 200*time.Millisecond)
+		defer m.Stop()
+
+		err := m.Add(uint64ToBytes(1))
+		require.NoError(t, err)
+		m.Add(uint64ToBytes(2))
+		require.NoError(t, err)
+		m.Add(uint64ToBytes(3))
+		require.NoError(t, err)
+
+		time.Sleep(250 * time.Millisecond) // wait for merge to happen
+
+		res, err := m.Get()
+		require.NoError(t, err)
+		require.Equal(t, uint64(6), bytesToUint64(res))
+	})
+}
+
+func TestMergeOperatorGetAfterStop(t *testing.T) {
+	key := []byte("merge")
+	runBadgerTest(t, nil, func(t *testing.T, db *DB) {
+		m := db.GetMergeOperator(key, add, 1*time.Second)
+
+		err := m.Add(uint64ToBytes(1))
+		require.NoError(t, err)
+		m.Add(uint64ToBytes(2))
+		require.NoError(t, err)
+		m.Add(uint64ToBytes(3))
+		require.NoError(t, err)
+
+		m.Stop()
+		res, err := m.Get()
+		require.NoError(t, err)
+		require.Equal(t, uint64(6), bytesToUint64(res))
+	})
+}
+
+func uint64ToBytes(i uint64) []byte {
+	var buf [8]byte
+	binary.BigEndian.PutUint64(buf[:], i)
+	return buf[:]
+}
+
+func bytesToUint64(b []byte) uint64 {
+	return binary.BigEndian.Uint64(b)
+}
+
+// Merge function to add two uint64 numbers
+func add(existing, new []byte) []byte {
+	return uint64ToBytes(bytesToUint64(existing) + bytesToUint64(new))
 }
