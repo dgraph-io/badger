6d504a871028a1dc917b50b15477769e61493cb2
Manish R Jain <manish@dgraph.io>
2019-05-14 14:43:50 -0700

    Remove the load to ram call to speed up table opening

--- table/table.go
+++ table/table.go
@@ -17,6 +17,7 @@
 package table
 
 import (
+	"bytes"
 	"crypto/sha256"
 	"encoding/binary"
 	"fmt"
@@ -140,25 +141,9 @@ func OpenTable(fd *os.File, mode options.FileLoadingMode, cksum []byte) (*Table,
 
 	t.tableSize = int(fileInfo.Size())
 
-	/*
-		// The following code block is intentionally commented out. Removing the loadToRAM()
-		// call lead to significant performance improvement (see
-		// https://github.com/dgraph-io/badger/pull/808). We will be adding block level checksum
-		// (currently we have a table level checksum) along with a block level cache in near future.
-
-		// We first load to RAM, so we can read the index and do checksum.
-		if err := t.loadToRAM(); err != nil {
-			return nil, err
-		}
-		// Enforce checksum before we read index. Otherwise, if the file was
-		// truncated, we'd end up with panics in readIndex.
-		if len(cksum) > 0 && !bytes.Equal(t.Checksum, cksum) {
-			return nil, fmt.Errorf(
-				"CHECKSUM_MISMATCH: Table checksum does not match checksum in MANIFEST."+
-					" NOT including table %s. This would lead to missing data."+
-					"\n  sha256 %x Expected\n  sha256 %x Found\n", filename, cksum, t.Checksum)
-		}
-	*/
+	// Removing the loadToRAM() call lead to significant performance improvement (see
+	// https://github.com/dgraph-io/badger/pull/808). We will be adding block level checksum
+	// (currently we have a table level checksum) along with a block level cache in near future.
 
 	if err := t.readIndex(); err != nil {
 		return nil, y.Wrap(err)
@@ -182,6 +167,15 @@ func OpenTable(fd *os.File, mode options.FileLoadingMode, cksum []byte) (*Table,
 	case options.LoadToRAM:
 		if err := t.loadToRAM(); err != nil {
 			return nil, err
+		}
+		// We already have the checksum for the table calculated. So, let's use it to ensure that
+		// file is intact. This isn't as great as doing a checksum before reading the index, but
+		// given we already have the checksum information, we might as well do it.
+		if len(cksum) > 0 && !bytes.Equal(t.Checksum, cksum) {
+			return nil, fmt.Errorf(
+				"CHECKSUM_MISMATCH: Table checksum does not match checksum in MANIFEST."+
+					" NOT including table %s. This would lead to missing data."+
+					"\n  sha256 %x Expected\n  sha256 %x Found\n", filename, cksum, t.Checksum)
 		}
 	case options.MemoryMap:
 		t.mmap, err = y.Mmap(fd, false, fileInfo.Size())
