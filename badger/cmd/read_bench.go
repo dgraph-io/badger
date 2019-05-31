d30f7bb0203f4cf5e13dc877a21b2557e8e011ea
ashish <ashish@dgraph.io>
2019-05-31 16:20:11 +0530

    Refactor Read Bench
    * Add sample-size and keys-only size
    * Fix code to have exactly sample no of keys
    * Wrap returned errors
    * Change function names
    * Add comments

--- badger/cmd/read_bench.go
+++ badger/cmd/read_bench.go
@@ -25,59 +25,73 @@ testing and performance analysis.
 	RunE: readBench,
 }
 
-const maxKeys = 1000
-
 var sizeRead, entriesRead uint64
 var startTime time.Time
+
+var sampleSize int
+var keysOnly bool
 
 func init() {
 	RootCmd.AddCommand(readBenchCmd)
 	readBenchCmd.Flags().IntVarP(
 		&numGoroutines, "goroutines", "g", 4, "Number of goroutines to run for reading.")
-	readBenchCmd.Flags().StringVarP(&duration, "duration", "d", "1m",
-		"How long to run the benchmark.")
+	readBenchCmd.Flags().StringVarP(
+		&duration, "duration", "d", "1m", "How long to run the benchmark.")
+	readBenchCmd.Flags().IntVar(
+		&sampleSize, "sample-size", 1000000, "Keys sample size to be used for random lookup.")
+	readBenchCmd.Flags().BoolVar(
+		&keysOnly, "keys-only", false, "If false, values will also be read.")
 }
 
 func readBench(cmd *cobra.Command, args []string) error {
 	dur, err := time.ParseDuration(duration)
-	y.Check(err)
+	if err != nil {
+		return y.Wrapf(err, "unable to parse duration")
+	}
 	y.AssertTrue(numGoroutines > 0)
 
 	opts := badger.DefaultOptions
 	opts.ReadOnly = true
 	opts.Dir = sstDir
 	opts.ValueDir = vlogDir
-	opts.TableLoadingMode = options.FileIO // TODO: make this configurable
+	// Table and value loading mode is fileIO here, as we want to test
+	// disk speed also. We can make this configurable in future.
+	opts.TableLoadingMode = options.FileIO
 	opts.ValueLogLoadingMode = options.FileIO
 
 	db, err := badger.Open(opts)
 	if err != nil {
-		return err
+		return y.Wrapf(err, "unable to open DB")
 	}
 	defer db.Close()
 
 	now := time.Now()
-	keys, err := getKeys(db)
+	keys, err := getSampleKeys(db)
 	if err != nil {
-		return err
+		return y.Wrapf(err, "error while sampling keys")
 	}
-	fmt.Printf("Total Keys: %d, read in time: %s\n", len(keys), time.Since(now))
+	fmt.Println("*********************************************************")
+	fmt.Printf("Total Sampled Keys: %d, read in time: %s\n", len(keys), time.Since(now))
+	fmt.Println("*********************************************************")
 
 	if len(keys) == 0 {
 		fmt.Println("DB is empty, hence returning")
 		return nil
 	}
 
+	fmt.Println("*********************************************************")
+	fmt.Println("Starting benchmarking Reads")
+	fmt.Println("*********************************************************")
 	c := y.NewCloser(0)
 	startTime = time.Now()
 	for i := 0; i < numGoroutines; i++ {
 		c.AddRunning(1)
-		go spawnRoutine(db, c, keys)
+		go spawnReader(db, c, keys)
 	}
 
-	// spawn printStats
+	// spawn stats reporter routine
 	c.AddRunning(1)
-	go printStats(c)
+	go spawnReporter(c)
 
 	<-time.After(dur)
 	c.SignalAndWait()
@@ -85,7 +99,7 @@ func readBench(cmd *cobra.Command, args []string) error {
 	return nil
 }
 
-func printStats(c *y.Closer) {
+func spawnReporter(c *y.Closer) {
 	defer c.Done()
 
 	t := time.NewTicker(time.Second)
@@ -110,7 +124,7 @@ func printStats(c *y.Closer) {
 	}
 }
 
-func spawnRoutine(db *badger.DB, c *y.Closer, keys [][]byte) {
+func spawnReader(db *badger.DB, c *y.Closer, keys [][]byte) {
 	defer c.Done()
 	r := rand.New(rand.NewSource(time.Now().Unix()))
 	for {
@@ -128,22 +142,23 @@ func spawnRoutine(db *badger.DB, c *y.Closer, keys [][]byte) {
 func lookupForKey(db *badger.DB, key []byte) (sz uint64) {
 	err := db.View(func(txn *badger.Txn) error {
 		itm, err := txn.Get(key)
-		if err != nil {
-			panic(err)
+		y.Check(err)
+
+		if keysOnly {
+			y.Check2(itm.ValueCopy(nil))
+			sz = uint64(itm.EstimatedSize())
+		} else {
+			sz = uint64(itm.KeySize())
 		}
-		if _, err := itm.ValueCopy(nil); err != nil {
-			panic(err)
-		}
-		sz = uint64(itm.EstimatedSize())
+
 		return nil
 	})
-	if err != nil {
-		panic(err)
-	}
+	y.Check(err)
 	return
 }
 
-func getKeys(db *badger.DB) ([][]byte, error) {
+// getSampleKeys uses stream framework internally, to get keys in random order.
+func getSampleKeys(db *badger.DB) ([][]byte, error) {
 	var keys [][]byte
 	count := 0
 	stream := db.NewStream()
@@ -153,11 +168,11 @@ func getKeys(db *badger.DB) ([][]byte, error) {
 	stream.Send = func(list *pb.KVList) error {
 		for _, kv := range list.Kv {
 			keys = append(keys, kv.Key)
-		}
-		count += len(list.Kv)
-		if count >= maxKeys {
-			// we only want maxKeys
-			cancel()
+			count++
+			if count >= sampleSize {
+				cancel()
+				break
+			}
 		}
 		return nil
 	}
