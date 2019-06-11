495641a542176efbceebe0b5a5f5b5ee28c74794
ashish <ashish@dgraph.io>
2019-06-11 18:22:20 +0530

    Rename fill to writeBench
    * Change file name fill to write_bench.go
    * Add code to print writestats

--- badger/cmd/write_bench.go
+++ badger/cmd/write_bench.go
@@ -18,46 +18,61 @@ package cmd
 
 import (
 	"encoding/binary"
+	"fmt"
 	"log"
 	"math/rand"
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/dgraph-io/badger"
 	"github.com/dgraph-io/badger/pb"
 	"github.com/dgraph-io/badger/y"
+	humanize "github.com/dustin/go-humanize"
 	"github.com/spf13/cobra"
 )
 
-var fillCmd = &cobra.Command{
-	Use:   "fill",
-	Short: "Fill Badger with random data.",
+var writeBenchCmd = &cobra.Command{
+	Use:   "write",
+	Short: "Writes random data to Badger to benchmark write speed.",
 	Long: `
-This command would fill Badger with random data. Useful for testing and performance analysis.
+This command writes random data to Badger to benchmark write speed. Useful for testing and 
+performance analysis.
 `,
-	RunE: fill,
+	RunE: writeBench,
 }
 
-var keySz, valSz int
-var numKeys float64
-var force, sorted bool
+var (
+	keySz   int
+	valSz   int
+	numKeys float64
+	force   bool
+	sorted  bool
 
-const mil float64 = 1e6
+	sizeWritten    uint64
+	entriesWritten uint64
+)
+
+const (
+	mil float64 = 1e6
+)
 
 func init() {
-	RootCmd.AddCommand(fillCmd)
-	fillCmd.Flags().IntVarP(&keySz, "key-size", "k", 32, "Size of key")
-	fillCmd.Flags().IntVarP(&valSz, "val-size", "v", 128, "Size of value")
-	fillCmd.Flags().Float64VarP(&numKeys, "keys-mil", "m", 10.0,
+	benchCmd.AddCommand(writeBenchCmd)
+	writeBenchCmd.Flags().IntVarP(&keySz, "key-size", "k", 32, "Size of key")
+	writeBenchCmd.Flags().IntVarP(&valSz, "val-size", "v", 128, "Size of value")
+	writeBenchCmd.Flags().Float64VarP(&numKeys, "keys-mil", "m", 10.0,
 		"Number of keys to add in millions")
-	fillCmd.Flags().BoolVarP(&force, "force-compact", "f", true, "Force compact level 0 on close.")
-	fillCmd.Flags().BoolVarP(&sorted, "sorted", "s", false, "Write keys in sorted order.")
+	writeBenchCmd.Flags().BoolVarP(&force, "force-compact", "f", true,
+		"Force compact level 0 on close.")
+	writeBenchCmd.Flags().BoolVarP(&sorted, "sorted", "s", false, "Write keys in sorted order.")
 }
 
-func fillRandom(db *badger.DB, num uint64) error {
+func writeRandom(db *badger.DB, num uint64) error {
 	value := make([]byte, valSz)
 	y.Check2(rand.Read(value))
 
+	es := uint64(keySz + valSz) // entry size is keySz + valSz
 	batch := db.NewWriteBatch()
 	for i := uint64(1); i <= num; i++ {
 		key := make([]byte, keySz)
@@ -65,16 +80,17 @@ func fillRandom(db *badger.DB, num uint64) error {
 		if err := batch.Set(key, value); err != nil {
 			return err
 		}
-		if i%1e5 == 0 {
-			log.Printf("Written keys: %d\n", i)
-		}
+
+		atomic.AddUint64(&entriesWritten, 1)
+		atomic.AddUint64(&sizeWritten, es)
 	}
 	return batch.Flush()
 }
 
-func fillSorted(db *badger.DB, num uint64) error {
+func writeSorted(db *badger.DB, num uint64) error {
 	value := make([]byte, valSz)
 	y.Check2(rand.Read(value))
+	es := 8 + valSz // key size is 8 bytes and value size is valSz
 
 	writer := db.NewStreamWriter()
 	if err := writer.Prepare(); err != nil {
@@ -97,7 +113,11 @@ func fillSorted(db *badger.DB, num uint64) error {
 				Version:  1,
 				StreamId: streamId,
 			})
-			sz += len(key) + len(value)
+
+			sz += es
+			atomic.AddUint64(&entriesWritten, 1)
+			atomic.AddUint64(&sizeWritten, uint64(es))
+
 			if sz >= 4<<20 { // 4 MB
 				writeCh <- kvs
 				kvs = &pb.KVList{}
@@ -109,21 +129,21 @@ func fillSorted(db *badger.DB, num uint64) error {
 
 	// Let's create some streams.
 	width := num / 16
-	streamId := uint32(0)
+	streamID := uint32(0)
 	for start := uint64(0); start < num; start += width {
 		end := start + width
 		if end > num {
 			end = num
 		}
-		streamId++
+		streamID++
 		wg.Add(1)
-		go writeRange(start, end, streamId)
+		go writeRange(start, end, streamID)
 	}
 	go func() {
 		wg.Wait()
 		close(writeCh)
 	}()
-	log.Printf("Max StreamId used: %d. Width: %d\n", streamId, width)
+	log.Printf("Max StreamId used: %d. Width: %d\n", streamID, width)
 	for kvs := range writeCh {
 		if err := writer.Write(kvs); err != nil {
 			panic(err)
@@ -133,7 +153,7 @@ func fillSorted(db *badger.DB, num uint64) error {
 	return writer.Flush()
 }
 
-func fill(cmd *cobra.Command, args []string) error {
+func writeBench(cmd *cobra.Command, args []string) error {
 	opts := badger.DefaultOptions
 	opts.Dir = sstDir
 	opts.ValueDir = vlogDir
@@ -148,17 +168,42 @@ func fill(cmd *cobra.Command, args []string) error {
 	defer func() {
 		start := time.Now()
 		err := db.Close()
-		log.Printf("DB.Close. Error: %v. Time taken: %s", err, time.Since(start))
+		log.Printf("DB.Close. Error: %v. Time taken to close: %s", err, time.Since(start))
 	}()
 
+	startTime = time.Now()
 	num := uint64(numKeys * mil)
-	start := time.Now()
-	defer func() {
-		dur := time.Since(start)
-		log.Printf("%d keys written. Time taken: %s\n", num, dur)
-	}()
+	c := y.NewCloser(1)
+	go reportStats(c)
+
 	if sorted {
-		return fillSorted(db, num)
+		err = writeSorted(db, num)
+	} else {
+		err = writeRandom(db, num)
 	}
-	return fillRandom(db, num)
+
+	c.SignalAndWait()
+	return err
+}
+
+func reportStats(c *y.Closer) {
+	defer c.Done()
+
+	t := time.NewTicker(time.Second)
+	defer t.Stop()
+	for {
+		select {
+		case <-c.HasBeenClosed():
+			return
+		case <-t.C:
+			dur := time.Since(startTime)
+			sz := atomic.LoadUint64(&sizeWritten)
+			entries := atomic.LoadUint64(&entriesWritten)
+			bytesRate := sz / uint64(dur.Seconds())
+			entriesRate := entries / uint64(dur.Seconds())
+			fmt.Printf("Time elapsed: %s, bytes written: %s, speed: %s/sec, "+
+				"entries written: %d, speed: %d/sec\n", y.FixedDuration(time.Since(startTime)),
+				humanize.Bytes(sz), humanize.Bytes(bytesRate), entries, entriesRate)
+		}
+	}
 }
