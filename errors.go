910d5db42767baefa675e0c3567101fd4251452e
பாலாஜி ஜின்னா <balaji@dgraph.io>
2019-08-12 03:09:09 -0700

    removing invalid error
    Signed-off-by: பாலாஜி ஜின்னா <balaji@dgraph.io>

--- errors.go
+++ errors.go
@@ -119,7 +119,4 @@ var (
 
 	// ErrInvalidDataKeyID is returned if the datakey id is invalid
 	ErrInvalidDataKeyID = errors.New("Invalid datakey id")
-
-	// ErrBadChecksum is returned if there is a checksum mismatch
-	ErrBadChecksum = errors.New("Checksum mismatch")
 )
