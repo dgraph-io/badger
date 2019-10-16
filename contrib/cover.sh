5a1d18888dd73943dbbd49d7d77500a94159fc4b
ashish <ashish@dgraph.io>
2019-10-16 15:45:50 +0530

    Add -v in travis go test

--- contrib/cover.sh
+++ contrib/cover.sh
@@ -6,14 +6,14 @@ TMP=$(mktemp /tmp/badger-coverage-XXXXX.txt)
 BUILD=$1
 OUT=$2
 
-set -e
+set -ex
 
 pushd $SRC &> /dev/null
 
 # create coverage output
 echo 'mode: atomic' > $OUT
 for PKG in $(go list ./...|grep -v -E 'vendor'); do
-  go test -covermode=atomic -coverprofile=$TMP $PKG
+  go test -v -covermode=atomic -coverprofile=$TMP $PKG
   tail -n +2 $TMP >> $OUT
 done
 
