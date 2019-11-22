ad770ca57a52f41078449885b7f064257124c6b7
Ibrahim Jarif <ibrahim@dgraph.io>
2019-11-22 16:52:26 +0530

    Fix request increment ref bug (#1121)
    The sendToWriteCh() method would increment the ref of a request by 2
    (one for the write and another for the publisher). The ref value was
    always decremented by the doWrite() function but the publisher would
    decrement it only if there were any subscribers.
    
    With this commit, we increment the request ref for the publisher only if
    there are any actual subscribers.

--- value.go
+++ value.go
@@ -1197,6 +1197,14 @@ type request struct {
 	ref  int32
 }
 
+func (req *request) reset() {
+	req.Entries = req.Entries[:0]
+	req.Ptrs = req.Ptrs[:0]
+	req.Wg = sync.WaitGroup{}
+	req.Err = nil
+	req.ref = 0
+}
+
 func (req *request) IncrRef() {
 	atomic.AddInt32(&req.ref, 1)
 }
@@ -1222,6 +1230,12 @@ type requests []*request
 func (reqs requests) DecrRef() {
 	for _, req := range reqs {
 		req.DecrRef()
+	}
+}
+
+func (reqs requests) IncrRef() {
+	for _, req := range reqs {
+		req.IncrRef()
 	}
 }
 
