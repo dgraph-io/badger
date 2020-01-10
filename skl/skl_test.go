baffaf0c12d45f5d44f7ec762bbbba9bbe03985f
Ibrahim Jarif <ibrahim@dgraph.io>
2020-01-10 16:04:25 +0530

    Add Concurrent Write benchmark

--- skl/skl_test.go
+++ skl/skl_test.go
@@ -516,3 +516,16 @@ func BenchmarkReadWriteMap(b *testing.B) {
 		})
 	}
 }
+
+func BenchmarkWrite(b *testing.B) {
+	value := newValue(123)
+	l := NewSkiplist(int64((b.N + 1) * MaxNodeSize))
+	defer l.DecrRef()
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		rng := rand.New(rand.NewSource(time.Now().UnixNano()))
+		for pb.Next() {
+			l.Put(randomKey(rng), y.ValueStruct{Value: value, Meta: 0, UserMeta: 0})
+		}
+	})
+}
