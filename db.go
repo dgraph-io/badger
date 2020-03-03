4500ae161737ab3df7b502a4ae77e4453a025e8b
Ibrahim Jarif <ibrahim@dgraph.io>
2020-03-03 13:33:04 +0530

    Fix ValueThreshold for in-memory mode (#1235)
    This commit increases the ValueThreshold for in-memory mode. The
    existing threshold was of 1 MB which means badger would crash if
    len(value) was greater than 1 MB. This commit sets the value threshold
    to MaxInt32 (around 2 GB). Badger transaction would return an error if
    badger is running in in-memory mode and length of value is greater than
    the value threshold.
    
    Fixes https://github.com/dgraph-io/badger/issues/1234
    
    (cherry picked from commit 5b4c0a65cdf94a30f63b87c7b1db7931ee6d4f4c)

--- db.go
+++ db.go
@@ -308,7 +308,8 @@ func Open(opt Options) (db *DB, err error) {
 
 	if db.opt.InMemory {
 		db.opt.SyncWrites = false
-		db.opt.ValueThreshold = maxValueThreshold
+		// If badger is running in memory mode, push everything into the LSM Tree.
+		db.opt.ValueThreshold = math.MaxInt32
 	}
 	krOpt := KeyRegistryOptions{
 		ReadOnly:                      opt.ReadOnly,
