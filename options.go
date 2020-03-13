1bcbefc03c753b0ad91a8cdf350532b42b47fb5a
Ibrahim Jarif <ibrahim@dgraph.io>
2020-03-13 21:05:29 +0530

    Add BypassDirLock option (#1243)
    Fixes https://github.com/dgraph-io/badger/issues/988
    Based on https://github.com/dgraph-io/badger/pull/1046
    
    This PR adds `BypassDirLock` option which allows badger to work without
    acquiring a lock on the data directory. This option could lead to data
    corruption if used with multiple badger instances trying to write to
    the same badger directory.
    
    Co-authored-by: Ehsan Noureddin Moosa <ehsannm@users.noreply.github.com>

--- options.go
+++ options.go
@@ -84,6 +84,11 @@ type Options struct {
 	EncryptionKey                 []byte        // encryption key
 	EncryptionKeyRotationDuration time.Duration // key rotation duration
 
+	// BypassLockGaurd will bypass the lock guard on badger. Bypassing lock
+	// guard can cause data corruption if multiple badger instances are using
+	// the same directory. Use this options with caution.
+	BypassLockGuard bool
+
 	// ChecksumVerificationMode decides when db should verify checksums for SSTable blocks.
 	ChecksumVerificationMode options.ChecksumVerificationMode
 
@@ -573,5 +578,18 @@ func (opt Options) WithInMemory(b bool) Options {
 // Benchmark code can be found in table/builder_test.go file
 func (opt Options) WithZSTDCompressionLevel(cLevel int) Options {
 	opt.ZSTDCompressionLevel = cLevel
+	return opt
+}
+
+// WithBypassLockGuard returns a new Options value with BypassLockGuard
+// set to the given value.
+//
+// When BypassLockGuard option is set, badger will not acquire a lock on the
+// directory. This could lead to data corruption if multiple badger instances
+// write to the same data directory. Use this option with caution.
+//
+// The default value of BypassLockGuard is false.
+func (opt Options) WithBypassLockGuard(b bool) Options {
+	opt.BypassLockGuard = b
 	return opt
 }
