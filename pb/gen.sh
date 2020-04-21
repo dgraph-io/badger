cddf7c03451c91e7a9c9df4121d16e1a56937786
Ibrahim Jarif <ibrahim@dgraph.io>
2020-04-21 11:56:06 +0530

    Proto: Rename dgraph.badger.v2.pb to badgerpb2 (#1314)
    This PR renames badger protobuf package from `dgraph.badger.v2.pb`
    to `badgerpb2`. 
    The `pb.pb.go` file has been regenerated using the `pb/gen.sh` script.

--- pb/gen.sh
+++ pb/gen.sh
@@ -5,3 +5,7 @@
 protos=${GOPATH-$HOME/go}/src/github.com/dgraph-io/badger/pb
 pushd $protos > /dev/null
 protoc --gofast_out=plugins=grpc:. -I=. pb.proto
+
+# Move pb.pb.go file to the correct directory. This is necessary because protoc
+# would generate the pb.pb.go file inside a different directory.
+mv $protos/github.com/dgraph-io/badger/v2/pb/pb.pb.go ./
