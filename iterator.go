1a2f636ae641d9b5392880c7503aac938dab3476
Anurag Sharma <anurags92@gmail.com>
2020-05-06 00:25:48 +0530

    Comment out error message

--- iterator.go
+++ iterator.go
@@ -169,8 +169,8 @@ func (item *Item) yieldItemValue() ([]byte, func(), error) {
 		vp.Decode(item.vptr)
 		result, cb, err := item.db.vlog.Read(vp, item.slice)
 		if err != ErrRetry {
-			item.db.opt.Logger.Errorf("Unable to read: Key: %v, Version : %v, meta: %v userMeta: %v",
-				key, item.version, item.meta, item.userMeta)
+			//item.db.opt.Logger.Errorf("Unable to read: Key: %v, Version : %v, meta: %v userMeta: %v",
+			//	key, item.version, item.meta, item.userMeta)
 			return result, cb, err
 		}
 		if bytes.HasPrefix(key, badgerMove) {
