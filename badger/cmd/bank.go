40dd2681161d598becc701b98d621221166b8aed
Ibrahim Jarif <ibrahim@dgraph.io>
2020-05-20 18:36:32 +0530

    Set keep l0 in memory to false

--- badger/cmd/bank.go
+++ badger/cmd/bank.go
@@ -364,6 +364,7 @@ func runTest(cmd *cobra.Command, args []string) error {
 		WithNumVersionsToKeep(int(math.MaxInt32)).
 		WithValueThreshold(1). // Make all values go to value log
 		WithCompression(options.ZSTD).
+		WithKeepL0InMemory(false).
 		WithMaxCacheSize(10 << 20)
 
 	if mmap {
