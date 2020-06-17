9d692067bdcd8a5c29611773df8d2215c0d53145
Ibrahim Jarif <ibrahim@dgraph.io>
2020-06-17 17:12:56 +0530

    Fix write batch crash

--- batch_test.go
+++ batch_test.go
@@ -70,6 +70,10 @@ func TestWriteBatch(t *testing.T) {
 		require.NoError(t, err)
 	}
 	t.Run("disk mode", func(t *testing.T) {
+		opt := getTestOptions("")
+		// Set value threshold to 32 bytes otherwise write batch will generate
+		// too many files and we will crash with too many files open error.
+		opt.ValueThreshold = 32
 		runBadgerTest(t, nil, func(t *testing.T, db *DB) {
 			test(t, db)
 		})
