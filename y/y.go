ee59e61fddc6667538d529eec3b0b19c85d46f10
Ibrahim Jarif <ibrahim@dgraph.io>
2020-07-08 18:49:57 +0530

    Avoid panic on multiple closer.Signal calls
    The closer.Signal function closes a channel and multiple calls to the
    closing the same channel would cause a "channel already closed". This PR
    avoids the panic by using a sync.Once while closing the channel.
    
    Multiple calls to closer.Signal would be no-op with this commit.

--- y/y.go
+++ y/y.go
@@ -191,8 +191,9 @@ func FixedDuration(d time.Duration) string {
 // to tell the goroutine to shut down, and a WaitGroup with which to wait for it to finish shutting
 // down.
 type Closer struct {
-	closed  chan struct{}
-	waiting sync.WaitGroup
+	closed    chan struct{}
+	waiting   sync.WaitGroup
+	closeOnce sync.Once
 }
 
 // NewCloser constructs a new Closer, with an initial count on the WaitGroup.
@@ -209,7 +210,9 @@ func (lc *Closer) AddRunning(delta int) {
 
 // Signal signals the HasBeenClosed signal.
 func (lc *Closer) Signal() {
-	close(lc.closed)
+	lc.closeOnce.Do(func() {
+		close(lc.closed)
+	})
 }
 
 // HasBeenClosed gets signaled when Signal() is called.
