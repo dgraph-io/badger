08a6b0157000c46d2989326672bad538787882ca
saurabh <ssuryavanshi@geekyants.com>
2020-07-28 17:54:18 +0530

    added version & updated links

--- docs/scripts/build.sh
+++ docs/scripts/build.sh
@@ -27,6 +27,7 @@ HUGO="${HUGO:-hugo}"
 # and then the older versions in descending order, such that the
 # build script can place the artifact in an appropriate location.
 VERSIONS_ARRAY=(
+	'v2.0.3'
 	'master'
 )
 
