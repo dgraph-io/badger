b43e3817725ff8bd44d507f04e4e0e8d6200b87f
Ahsan Barkati <ahsanbarkati@gmail.com>
2020-08-14 14:44:00 +0530

    Fix iterator and transaction closure in write benchmark

--- badger/cmd/write_bench.go
+++ badger/cmd/write_bench.go
@@ -71,6 +71,7 @@ var (
 	compression         bool
 	showDir             bool
 	ttlDuration         string
+	showKeysCount       bool
 
 	internalKeyCount uint32
 	moveKeyCount     uint32
@@ -133,6 +134,8 @@ func init() {
 		"TTL duration in seconds for the entries, 0 means without TTL")
 	writeBenchCmd.Flags().StringVarP(&gcPeriod, "gc-every", "g", "5m", "GC Period.")
 	writeBenchCmd.Flags().Float64VarP(&gcDiscardRatio, "gc-ratio", "r", 0.5, "GC discard ratio.")
+	writeBenchCmd.Flags().BoolVar(&showKeysCount, "show-keys", false,
+		"If true, the report will include the keys statistics")
 }
 
 func writeRandom(db *badger.DB, num uint64) error {
@@ -309,28 +312,35 @@ func reportStats(c *y.Closer, db *badger.DB) {
 		case <-c.HasBeenClosed():
 			return
 		case <-t.C:
-			txn := db.NewTransaction(false)
-			defer txn.Discard()
+			if showKeysCount {
+				func() {
+					txn := db.NewTransaction(false)
+					defer txn.Discard()
 
-			iopt := badger.DefaultIteratorOptions
-			iopt.AllVersions = true
-			iopt.InternalAccess = true
+					iopt := badger.DefaultIteratorOptions
+					iopt.AllVersions = true
+					iopt.InternalAccess = true
+					it := txn.NewIterator(iopt)
+					defer it.Close()
 
-			it := txn.NewIterator(iopt)
-			defer it.Close()
-			for it.Rewind(); it.Valid(); it.Next() {
-				i := it.Item()
-				if bytes.HasPrefix(i.Key(), []byte("!badger!")) {
-					internalKeyCount++
-				}
-				if bytes.HasPrefix(i.Key(), []byte("!badger!Move")) {
-					moveKeyCount++
-				}
-				if i.IsDeletedOrExpired() {
-					invalidKeyCount++
-				} else {
-					validKeyCount++
-				}
+					for it.Rewind(); it.Valid(); it.Next() {
+						i := it.Item()
+						if bytes.HasPrefix(i.Key(), []byte("!badger!")) {
+							internalKeyCount++
+						}
+						if bytes.HasPrefix(i.Key(), []byte("!badger!Move")) {
+							moveKeyCount++
+						}
+						if i.IsDeletedOrExpired() {
+							invalidKeyCount++
+						} else {
+							validKeyCount++
+						}
+					}
+				}()
+				fmt.Printf("Valid Keys Count: %d\nInvalid Keys Count: %d\nMove Keys Count:"+
+					"%d\nInternal Keys Count: %d\n", validKeyCount, invalidKeyCount,
+					moveKeyCount, internalKeyCount)
 			}
 
 			// fetch directory contents
@@ -365,9 +375,6 @@ func reportStats(c *y.Closer, db *badger.DB) {
 			fmt.Printf("Time elapsed: %s, bytes written: %s, speed: %s/sec, "+
 				"entries written: %d, speed: %d/sec, gcSuccess: %d\n", y.FixedDuration(time.Since(startTime)),
 				humanize.Bytes(sz), humanize.Bytes(bytesRate), entries, entriesRate, gcSuccess)
-			fmt.Printf("Valid Keys Count: %d\nInvalid Keys Count: %d\nMove Keys Count: %d\n"+
-				"Internal Keys Count: %d\n", validKeyCount, invalidKeyCount, moveKeyCount,
-				internalKeyCount)
 		}
 	}
 }
