319ba23b9e754f3d59f17c905b32874db0770c8a
Manish R Jain <manish@dgraph.io>
2020-08-19 09:25:58 -0700

    Have a way to add slices.

--- y/buffer.go
+++ y/buffer.go
@@ -1,5 +1,7 @@
 package y
 
+import "encoding/binary"
+
 type Buffer struct {
 	buf    []byte
 	offset int
@@ -10,6 +12,10 @@ func NewBuffer(sz int) *Buffer {
 		buf:    Calloc(sz),
 		offset: 0,
 	}
+}
+
+func (b *Buffer) Len() int {
+	return b.offset
 }
 
 func (b *Buffer) Bytes() []byte {
@@ -37,6 +43,41 @@ func (b *Buffer) Grow(n int) {
 	copy(newBuf, b.buf[:b.offset])
 	Free(b.buf)
 	b.buf = newBuf
+}
+
+// Allocate is not thread-safe. The byte slice returned MUST be used before further calls to Buffer.
+func (b *Buffer) Allocate(n int) []byte {
+	b.Grow(n)
+	off := b.offset
+	b.offset += n
+	return b.buf[off:b.offset]
+}
+
+func (b *Buffer) WriteLen(sz int) {
+	buf := b.Allocate(4)
+	binary.BigEndian.PutUint32(buf, uint32(sz))
+}
+
+func (b *Buffer) AllocateSlice(sz int) []byte {
+	b.Grow(4 + sz)
+	b.WriteLen(sz)
+	return b.Allocate(sz)
+}
+
+func (b *Buffer) SliceOffsets(offsets []int) []int {
+	start := 0
+	for start < b.offset {
+		offsets = append(offsets, start)
+		sz := binary.BigEndian.Uint32(b.buf[start:])
+		start += 4 + int(sz)
+	}
+	return offsets
+}
+
+func (b *Buffer) Slice(offset int) []byte {
+	sz := binary.BigEndian.Uint32(b.buf[offset:])
+	start := offset + 4
+	return b.buf[start : start+int(sz)]
 }
 
 func (b *Buffer) Write(p []byte) (n int, err error) {
