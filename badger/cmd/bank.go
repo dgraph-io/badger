14fbc038d5d1bd94833111c4568db0b3d10bc86f
rahulgurnani <rahulgurnani09@gmail.com>
2020-10-30 21:56:39 +0530

    Add index cache

--- badger/cmd/bank.go
+++ badger/cmd/bank.go
@@ -360,6 +360,7 @@ func runTest(cmd *cobra.Command, args []string) error {
 		WithBaseTableSize(4 << 20). // Force more compactions.
 		WithNumLevelZeroTables(2).
 		WithNumMemtables(2).
+		WithIndexCacheSize(10 << 20).
 		// Do not GC any versions, because we need them for the disect..
 		WithNumVersionsToKeep(int(math.MaxInt32)).
 		WithValueThreshold(1). // Make all values go to value log
