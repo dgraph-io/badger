3adc574c149019f3373fc4fbfd252c38d5598c08
Ibrahim Jarif <ibrahim@dgraph.io>
2020-12-23 14:58:06 +0530

    fix(badger): Do not reuse variable across badger commands (#1624)
    The same variables were being across multiple badger commands. The
    default value of a flag that uses the same variable across two commands
    could be from one of the commands and it isn't clear which default value
    would be used.
    
    The numVersions command was being used in flatten (default value 1)
    and stream (default value 0). Running badger stream --help would show
    the default value to be 0 but if you print the value of the
    numVersions variable, it would turn out to be 1. This is misleading.
    
    This PR separates the variable so that they don't overlap.
    
    This PR also adds the compression flag to the flatten tool.

--- badger/cmd/flatten.go
+++ badger/cmd/flatten.go
@@ -21,6 +21,8 @@ import (
 	"math"
 
 	"github.com/dgraph-io/badger/v2"
+	"github.com/dgraph-io/badger/v2/options"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
@@ -33,36 +35,48 @@ This command would compact all the LSM tables into one level.
 	RunE: flatten,
 }
 
-var keyPath string
-var numWorkers int
+var fo = struct {
+	keyPath         string
+	numWorkers      int
+	numVersions     int
+	compressionType uint32
+}{}
 
 func init() {
 	RootCmd.AddCommand(flattenCmd)
-	flattenCmd.Flags().IntVarP(&numWorkers, "num-workers", "w", 1,
+	flattenCmd.Flags().IntVarP(&fo.numWorkers, "num-workers", "w", 1,
 		"Number of concurrent compactors to run. More compactors would use more"+
 			" server resources to potentially achieve faster compactions.")
-	flattenCmd.Flags().IntVarP(&numVersions, "num_versions", "", 1,
+	flattenCmd.Flags().IntVarP(&fo.numVersions, "num_versions", "", 0,
 		"Option to configure the maximum number of versions per key. "+
 			"Values <= 0 will be considered to have the max number of versions.")
-	flattenCmd.Flags().StringVar(&keyPath, "encryption-key-file", "",
+	flattenCmd.Flags().StringVar(&fo.keyPath, "encryption-key-file", "",
 		"Path of the encryption key file.")
+	flattenCmd.Flags().Uint32VarP(&fo.compressionType, "compression", "", 1,
+		"Option to configure the compression type in output DB. "+
+			"0 to disable, 1 for Snappy, and 2 for ZSTD.")
 }
 
 func flatten(cmd *cobra.Command, args []string) error {
-	if numVersions <= 0 {
+	if fo.numVersions <= 0 {
 		// Keep all versions.
-		numVersions = math.MaxInt32
+		fo.numVersions = math.MaxInt32
 	}
-	encKey, err := getKey(keyPath)
+	encKey, err := getKey(fo.keyPath)
 	if err != nil {
 		return err
 	}
+	if fo.compressionType < 0 || fo.compressionType > 2 {
+		return errors.Errorf(
+			"compression value must be one of 0 (disabled), 1 (Snappy), or 2 (ZSTD)")
+	}
 	opt := badger.DefaultOptions(sstDir).
 		WithValueDir(vlogDir).
-		WithNumVersionsToKeep(numVersions).
+		WithNumVersionsToKeep(fo.numVersions).
 		WithNumCompactors(0).
 		WithBlockCacheSize(100 << 20).
 		WithIndexCacheSize(200 << 20).
+		WithCompression(options.CompressionType(fo.compressionType)).
 		WithEncryptionKey(encKey)
 	fmt.Printf("Opening badger with options = %+v\n", opt)
 	db, err := badger.Open(opt)
@@ -71,5 +85,5 @@ func flatten(cmd *cobra.Command, args []string) error {
 	}
 	defer db.Close()
 
-	return db.Flatten(numWorkers)
+	return db.Flatten(fo.numWorkers)
 }
