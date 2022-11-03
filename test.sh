1a989969fbfa5f1401e328a6e59e4b92579a807e
Joshua Goldstein <92491720+joshua-goldstein@users.noreply.github.com>
2022-11-03 17:50:45 -0500

    chore(ci): run jobs on release branch (#1829)
    Run CI jobs on release branch.

--- test.sh
+++ test.sh
@@ -4,47 +4,26 @@ set -eo pipefail
 
 go version
 
+# Check if Github Actions is running
+if [ $CI = "true" ]; then
+  # Enable code coverage
+  # export because tests run in a subprocess
+  export covermode="-covermode=atomic"
+  export coverprofile="-coverprofile=cover_tmp.out"
+  echo "mode: atomic" >> cover.out
+fi
+
 # Run `go list` BEFORE setting GOFLAGS so that the output is in the right
 # format for grep.
 # export packages because the test will run in a sub process.
 export packages=$(go list ./... | grep "github.com/dgraph-io/badger/v3/")
 
-if [[ ! -z "$TEAMCITY_VERSION" ]]; then
-  export GOFLAGS="-json"
-fi
-
-function InstallJemalloc() {
-  pushd .
-  if [ ! -f /usr/local/lib/libjemalloc.a ]; then
-		USER_ID=`id -u`
-    	JEMALLOC_URL="https://github.com/jemalloc/jemalloc/releases/download/5.2.1/jemalloc-5.2.1.tar.bz2"
-
-		mkdir -p /tmp/jemalloc-temp && cd /tmp/jemalloc-temp ;
-		echo "Downloading jemalloc" ;
-		curl -s -L ${JEMALLOC_URL} -o jemalloc.tar.bz2 ;
-		tar xjf ./jemalloc.tar.bz2 ;
-		cd jemalloc-5.2.1 ;
-		./configure --with-jemalloc-prefix='je_' ;
-		make ;
-		if [ "$USER_ID" -eq "0" ]; then
-			make install ;
-		else
-			echo "==== Need sudo access to install jemalloc" ;
-			sudo make install ;
-		fi
-	fi
-  popd
-}
-
 tags="-tags=jemalloc"
 
-# Ensure that we can compile the binary.
+# Compile the Badger binary
 pushd badger
 go build -v $tags .
 popd
-
-# tags=""
-InstallJemalloc
 
 # Run the memory intensive tests first.
 manual() {
@@ -53,7 +32,7 @@ manual() {
   set -e
   for pkg in $packages; do
     echo "===> Testing $pkg"
-    go test $tags -timeout=25m -race $pkg -parallel 16
+    go test $tags -timeout=25m $covermode $coverprofile -race -parallel 16 $pkg && write_coverage
   done
   echo "==> DONE package tests"
 
@@ -61,10 +40,10 @@ manual() {
   # Run the special Truncate test.
   rm -rf p
   set -e
-  go test $tags $timeout -run='TestTruncateVlogNoClose$' --manual=true
+  go test $tags $timeout $covermode $coverprofile -run='TestTruncateVlogNoClose$' --manual=true && write_coverage
   truncate --size=4096 p/000000.vlog
-  go test $tags $timeout -run='TestTruncateVlogNoClose2$' --manual=true
-  go test $tags $timeout -run='TestTruncateVlogNoClose3$' --manual=true
+  go test $tags $timeout $covermode $coverprofile -run='TestTruncateVlogNoClose2$' --manual=true && write_coverage
+  go test $tags $timeout $covermode $coverprofile -run='TestTruncateVlogNoClose3$' --manual=true && write_coverage
   rm -rf p
 
   # TODO(ibrahim): Let's make these tests have Manual prefix.
@@ -73,14 +52,14 @@ manual() {
   # TestValueGCManaged
   # TestDropPrefix
   # TestDropAllManaged
-  go test $tags $timeout -run='TestBigKeyValuePairs$' --manual=true
-  go test $tags $timeout -run='TestPushValueLogLimit' --manual=true
-  go test $tags $timeout -run='TestKeyCount' --manual=true
-  go test $tags $timeout -run='TestIteratePrefix' --manual=true
-  go test $tags $timeout -run='TestIterateParallel' --manual=true
-  go test $tags $timeout -run='TestBigStream' --manual=true
-  go test $tags $timeout -run='TestGoroutineLeak' --manual=true
-  go test $tags $timeout -run='TestGetMore' --manual=true
+  go test $tags $timeout $covermode $coverprofile -run='TestBigKeyValuePairs$' --manual=true && write_coverage
+  go test $tags $timeout $covermode $coverprofile -run='TestPushValueLogLimit' --manual=true && write_coverage
+  go test $tags $timeout $covermode $coverprofile -run='TestKeyCount' --manual=true && write_coverage
+  go test $tags $timeout $covermode $coverprofile -run='TestIteratePrefix' --manual=true && write_coverage
+  go test $tags $timeout $covermode $coverprofile -run='TestIterateParallel' --manual=true && write_coverage
+  go test $tags $timeout $covermode $coverprofile -run='TestBigStream' --manual=true && write_coverage
+  go test $tags $timeout $covermode $coverprofile -run='TestGoroutineLeak' --manual=true && write_coverage
+  go test $tags $timeout $covermode $coverprofile -run='TestGetMore' --manual=true && write_coverage
 
   echo "==> DONE manual tests"
 }
@@ -91,7 +70,7 @@ root() {
 
   echo "==> Running root level tests."
   set -e
-  go test $tags -timeout=25m . -v -race -parallel 16
+  go test $tags -v -race -parallel=16 -timeout=25m $covermode $coverprofile . && write_coverage
   echo "==> DONE root level tests"
 }
 
@@ -110,11 +89,23 @@ stream() {
     return 1
   fi
   echo "==> DONE stream test"
+  popd
   return 0
 }
 
-export -f stream
-export -f manual
-export -f root
+write_coverage() {
+  if [ $CI = "true" ]; then
+    if [ -f cover_tmp.out ]; then
+      sed -i '1d' cover_tmp.out
+      cat cover_tmp.out >> cover.out && rm cover_tmp.out
+    fi
+  fi
+  
+}
 
-parallel --halt now,fail=1 --progress --line-buffer ::: stream manual root
+# parallel tests currently not working
+# parallel --halt now,fail=1 --progress --line-buffer ::: stream manual root
+# run tests in sequence
+root
+stream
+manual
