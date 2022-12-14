d20b1c982c6f37f859b07add65995b67571f6aa1
Joshua Goldstein <jgoldstein345@gmail.com>
2022-12-14 15:02:52 -0600

    update runner script

--- contrib/gh-runner.sh
+++ contrib/gh-runner.sh
@@ -13,19 +13,19 @@ mkdir actions-runner && cd actions-runner
 if [ "$(uname -m)" = "aarch64" ]; then
     echo "Detected arm64 architecture"
     # Download the latest runner package
-    curl -o actions-runner-linux-arm64-2.298.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.298.2/actions-runner-linux-arm64-2.298.2.tar.gz
+    curl -o actions-runner-linux-arm64-2.299.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.299.1/actions-runner-linux-arm64-2.299.1.tar.gz
     # Optional: Validate the hash
-    echo "803e4aba36484ef4f126df184220946bc151ae1bbaf2b606b6e2f2280f5042e8  actions-runner-linux-arm64-2.298.2.tar.gz" | shasum -a 256 -c
+    echo "debe1cc9656963000a4fbdbb004f475ace5b84360ace2f7a191c1ccca6a16c00  actions-runner-linux-arm64-2.299.1.tar.gz" | shasum -a 256 -c
     # Extract the installer
-    tar xzf ./actions-runner-linux-arm64-2.298.2.tar.gz
+    tar xzf ./actions-runner-linux-arm64-2.299.1.tar.gz
 elif [ "$(uname -m)" = "x86_64" ]; then
     echo "Detected amd64 architecture"
     # Download the latest runner package
-    curl -o actions-runner-linux-x64-2.298.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.298.2/actions-runner-linux-x64-2.298.2.tar.gz
+    curl -o actions-runner-linux-x64-2.299.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.299.1/actions-runner-linux-x64-2.299.1.tar.gz
     # Optional: Validate the hash
-    echo "0bfd792196ce0ec6f1c65d2a9ad00215b2926ef2c416b8d97615265194477117  actions-runner-linux-x64-2.298.2.tar.gz" | shasum -a 256 -c 
+    echo "147c14700c6cb997421b9a239c012197f11ea9854cd901ee88ead6fe73a72c74  actions-runner-linux-x64-2.299.1.tar.gz" | shasum -a 256 -c
     # Extract the installer
-    tar xzf ./actions-runner-linux-x64-2.298.2.tar.gz
+    tar xzf ./actions-runner-linux-x64-2.299.1.tar.gz
 else
     echo "Unrecognized architecture"
     exit 1
