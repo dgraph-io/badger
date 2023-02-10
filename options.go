62b05e6d5e1ff7ece8fecbab39fdc3bf53df139e
Joshua Goldstein <92491720+joshua-goldstein@users.noreply.github.com>
2023-02-10 14:33:01 -0600

    Update options.go

--- options.go
+++ options.go
@@ -476,7 +476,6 @@ func (opt Options) WithBaseTableSize(val int64) Options {
 //
 // LevelSizeMultiplier sets the ratio between the maximum sizes of contiguous levels in the LSM.
 // Once a level grows to be larger than this ratio allowed, the compaction process will be
-//
 // triggered.
 //
 // The default value of LevelSizeMultiplier is 10.
