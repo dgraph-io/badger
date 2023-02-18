fbe78e4febde0b7519359a3c3273aed2ae2d790c
Joshua Goldstein <jgoldstein345@gmail.com>
2023-02-18 11:15:00 -0600

    add test

---
+++ structs_test.go
@@ -0,0 +1,17 @@
+package badger
+
+import (
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// Regression test for github.com/dgraph-io/badger/pull/1800
+func TestLargeEncode(t *testing.T) {
+
+	var headerEnc [maxHeaderSize]byte
+	h := header{math.MaxUint32, math.MaxUint32, math.MaxUint64, math.MaxUint8, math.MaxUint8}
+	require.NotPanics(t, func() { _ = h.Encode(headerEnc[:]) })
+
+}
