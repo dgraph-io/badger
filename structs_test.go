f30306fc0e830ba51752e776531247e95fa73e3c
Joshua Goldstein <92491720+joshua-goldstein@users.noreply.github.com>
2023-02-19 17:40:06 -0600

    fix: update maxHeaderSize to 22 #1800 (#1877)

---
+++ structs_test.go
@@ -0,0 +1,21 @@
+package badger
+
+import (
+	"math"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// Regression test for github.com/dgraph-io/badger/pull/1800
+func TestLargeEncode(t *testing.T) {
+	var headerEnc [maxHeaderSize]byte
+	h := header{math.MaxUint32, math.MaxUint32, math.MaxUint64, math.MaxUint8, math.MaxUint8}
+	require.NotPanics(t, func() { _ = h.Encode(headerEnc[:]) })
+}
+
+func TestNumFieldsHeader(t *testing.T) {
+	// maxHeaderSize must correspond with any changes made to header
+	require.Equal(t, 5, reflect.TypeOf(header{}).NumField())
+}
