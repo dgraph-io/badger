e7a17ee6c5144862e524739c029908dae16bc207
Siddhant Jain <siddhant.jain120@gmail.com>
2023-08-22 11:26:42 +0530

    Merger commit bug fix

--- rollup_test/main.go
+++ rollup_test/main.go
@@ -3,42 +3,55 @@ package main
 import (
 	"fmt"
 	"log"
+	"sync"
+	"sync/atomic"
 	"time"
 
 	badger "github.com/dgraph-io/badger/v4"
 )
+
+var atomicTs int64
 
 func adder(db *badger.DB) {
 
 	key := "test"
 
 	for i := int32(0); i < 20; i++ {
-		time.Sleep(50 * time.Millisecond)
-		fmt.Printf("Adding delta: %d. At time: %d\n", i, time.Now().UnixMilli())
+		time.Sleep(10 * time.Millisecond)
 
 		var curr delta
-		if i < 10 {
-			curr.set_op = true
-			curr.del_op = false
-			curr.consolidated = false
-			curr.last_version = 0
-			curr.values = append(curr.values, i, 10+i)
-		} else {
-			curr.set_op = false
-			curr.del_op = true
-			curr.consolidated = false
-			curr.last_version = 0
-			curr.values = append(curr.values, i, i-10)
-		}
-		err := db.Update(func(txn *badger.Txn) error {
-			currBytes, _ := structToBytes(curr)
-			txn.Set([]byte(key), currBytes)
-			return nil
+		// if i < 10 {
+		curr.set_op = true
+		curr.del_op = false
+		curr.consolidated = false
+		curr.last_version = 0
+		curr.values = append(curr.values, i)
+		// } else {
+		// 	curr.set_op = false
+		// 	curr.del_op = true
+		// 	curr.consolidated = false
+		// 	curr.last_version = 0
+		// 	curr.values = append(curr.values, i, i-10)
+		// }
+
+		ts := atomic.LoadInt64(&atomicTs)
+		fmt.Printf("Adding delta: %d. At time: %d\n", i, ts+1)
+
+		txn := db.NewTransactionAt(uint64(ts), true)
+
+		currBytes, _ := structToBytes(curr)
+		txn.Set([]byte(key), currBytes)
+
+		var wg sync.WaitGroup
+		wg.Add(1)
+
+		txn.CommitAt(uint64(ts+1), func(err error) {
+			atomic.AddInt64(&atomicTs, 2)
+			wg.Done()
 		})
 
-		if err != nil {
-			log.Fatal(err)
-		}
+		wg.Wait()
+
 	}
 }
 
@@ -81,82 +94,110 @@ func getter(db *badger.DB) {
 
 func merger(db *badger.DB) {
 	// skipCounter := 0
+	time.Sleep(2 * time.Millisecond)
+	for i := 0; i < 20; i++ {
+		time.Sleep(20 * time.Millisecond)
 
-	for i := 0; i < 40; i++ {
-		time.Sleep(100 * time.Millisecond)
-		fmt.Printf("Merging deltas till now at time: %d\n", time.Now().UnixMilli())
+		ts := atomic.LoadInt64(&atomicTs)
+		if ts < 2 {
+			continue
+		}
+		fmt.Printf("Merging deltas till now at time: %d\n", ts)
 
-		_ = db.Update(func(txn *badger.Txn) error {
-			opt := badger.DefaultIteratorOptions
-			// opt.Reverse = true
-			it := txn.NewKeyIterator([]byte("test"), opt)
-			defer it.Close()
+		txn := db.NewTransactionAt(uint64(ts), false)
 
-			earliestTimestamp := 0
-			var consolidated delta
+		opt := badger.DefaultIteratorOptions
+		// opt.Reverse = true
 
-			consolidated.consolidated = true
-			consolidated.set_op = false
-			consolidated.del_op = false
-			consolidated.last_version = 0
+		it := txn.NewKeyIterator([]byte("test"), opt)
+		defer it.Close()
 
-			var allDeltas []delta
-			latestVersion := uint64(0)
+		// earliestTimestamp := 0
+		var consolidated delta
 
-			for it.Seek([]byte("test")); it.Valid(); it.Next() {
-				item := it.Item()
-				version := item.Version()
+		consolidated.consolidated = true
+		consolidated.set_op = false
+		consolidated.del_op = false
+		consolidated.last_version = 0
 
-				if version > latestVersion {
-					latestVersion = version
-				}
+		var allDeltas []delta
+		latestVersion := uint64(0)
 
-				if version <= uint64(earliestTimestamp) {
-					break
-				}
+		for it.Seek([]byte("test")); it.Valid(); it.Next() {
+			item := it.Item()
+			version := item.Version()
 
-				valCopy, err := item.ValueCopy(nil)
-				if err != nil {
-					continue
-				}
-
-				itemValue, _ := bytesToStruct(valCopy)
-
-				for _, num := range itemValue.values {
-					fmt.Printf("%d, ", num)
-				}
-				fmt.Println("")
-
-				if itemValue.consolidated == true {
-					earliestTimestamp = int(itemValue.last_version)
-					consolidated = itemValue
-				} else {
-					allDeltas = append(allDeltas, itemValue)
-				}
+			if version > latestVersion {
+				latestVersion = version
 			}
 
-			for i := len(allDeltas) - 1; i >= 0; i-- {
-				consolidated = mergeDelta(consolidated, allDeltas[i])
+			// if version <= uint64(earliestTimestamp) {
+			// 	break
+			// }
+
+			valCopy, err := item.ValueCopy(nil)
+			if err != nil {
+				continue
 			}
-			consolidated.last_version = int32(latestVersion)
 
-			consolidatedBytes, _ := structToBytes(consolidated)
-			txn.Set([]byte("test"), consolidatedBytes)
+			itemValue, _ := bytesToStruct(valCopy)
 
-			return nil
+			// fmt.Println("Delta item value: ", itemValue, )
+			fmt.Printf("%d: ", version)
+			for _, num := range itemValue.values {
+				fmt.Printf("%d, ", num)
+			}
+			fmt.Println("")
+
+			if itemValue.consolidated == true {
+				// earliestTimestamp = int(itemValue.last_version)
+				consolidated = itemValue
+				break
+			} else {
+				allDeltas = append(allDeltas, itemValue)
+			}
+		}
+
+		for i := len(allDeltas) - 1; i >= 0; i-- {
+			consolidated = mergeDelta(consolidated, allDeltas[i])
+		}
+		consolidated.last_version = int32(latestVersion)
+		fmt.Println(consolidated)
+		consolidatedBytes, _ := structToBytes(consolidated)
+		it.Close()
+
+		txn.Discard()
+
+		var wg sync.WaitGroup
+		wg.Add(1)
+
+		txn = db.NewTransactionAt(uint64(ts), true)
+		defer txn.Discard()
+		txn.Set([]byte("test"), consolidatedBytes)
+		txn.CommitAt(uint64(latestVersion), func(err error) {
+			if err == nil {
+				wg.Done()
+			} else {
+				log.Fatal(err)
+			}
 		})
 
+		wg.Wait()
+
+		fmt.Printf("Committed rollup %d at %d\n", ts, latestVersion)
 	}
 }
 
 func main() {
-	db, err := badger.Open(badger.DefaultOptions("/tmp/badger_threaded36"))
+	db, err := badger.OpenManaged(badger.DefaultOptions("/tmp/badger"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
+	atomicTs = 1
 	go adder(db)
 	merger(db)
+
 	// getter(db)
 }
