3976c0716dabab51195470c5d1d636200552f975
Ryan Fox-Tyler <60440289+ryanfoxtyler@users.noreply.github.com>
2025-02-05 14:37:05 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 go version
 
 # Check if Github Actions is running
-if [[ ${CI} == "true" ]]; then
+if [ $CI = "true" ]; then
 	# Enable code coverage
 	# export because tests run in a subprocess
 	export covermode="-covermode=atomic"
@@ -22,17 +22,17 @@ tags="-tags=jemalloc"
 
 # Compile the Badger binary
 pushd badger
-go build -v "${tags}" .
+go build -v $tags .
 popd
 
 # Run the memory intensive tests first.
 manual() {
 	timeout="-timeout 2m"
-	echo "==> Running package tests for ${packages}"
+	echo "==> Running package tests for $packages"
 	set -e
-	for pkg in ${packages}; do
-		echo "===> Testing ${pkg}"
-		go test "${tags}" -timeout=25m "${covermode}" "${coverprofile}" -failfast -race -parallel 16 "${pkg}" && write_coverage || return 1
+	for pkg in $packages; do
+		echo "===> Testing $pkg"
+		go test $tags -timeout=25m $covermode $coverprofile -failfast -race -parallel 16 $pkg && write_coverage || return 1
 	done
 	echo "==> DONE package tests"
 
@@ -40,10 +40,10 @@ manual() {
 	# Run the special Truncate test.
 	rm -rf p
 	set -e
-	go test "${tags}" "${timeout}" "${covermode}" "${coverprofile}" -run='TestTruncateVlogNoClose$' -failfast --manual=true && write_coverage || return 1
+	go test $tags $timeout $covermode $coverprofile -run='TestTruncateVlogNoClose$' -failfast --manual=true && write_coverage || return 1
 	truncate --size=4096 p/000000.vlog
-	go test "${tags}" "${timeout}" "${covermode}" "${coverprofile}" -run='TestTruncateVlogNoClose2$' -failfast --manual=true && write_coverage || return 1
-	go test "${tags}" "${timeout}" "${covermode}" "${coverprofile}" -run='TestTruncateVlogNoClose3$' -failfast --manual=true && write_coverage || return 1
+	go test $tags $timeout $covermode $coverprofile -run='TestTruncateVlogNoClose2$' -failfast --manual=true && write_coverage || return 1
+	go test $tags $timeout $covermode $coverprofile -run='TestTruncateVlogNoClose3$' -failfast --manual=true && write_coverage || return 1
 	rm -rf p
 
 	# TODO(ibrahim): Let's make these tests have Manual prefix.
@@ -52,14 +52,14 @@ manual() {
 	# TestValueGCManaged
 	# TestDropPrefix
 	# TestDropAllManaged
-	go test "${tags}" "${timeout}" "${covermode}" "${coverprofile}" -failfast -run='TestBigKeyValuePairs$' --manual=true && write_coverage || return 1
-	go test "${tags}" "${timeout}" "${covermode}" "${coverprofile}" -failfast -run='TestPushValueLogLimit' --manual=true && write_coverage || return 1
-	go test "${tags}" "${timeout}" "${covermode}" "${coverprofile}" -failfast -run='TestKeyCount' --manual=true && write_coverage || return 1
-	go test "${tags}" "${timeout}" "${covermode}" "${coverprofile}" -failfast -run='TestIteratePrefix' --manual=true && write_coverage || return 1
-	go test "${tags}" "${timeout}" "${covermode}" "${coverprofile}" -failfast -run='TestIterateParallel' --manual=true && write_coverage || return 1
-	go test "${tags}" "${timeout}" "${covermode}" "${coverprofile}" -failfast -run='TestBigStream' --manual=true && write_coverage || return 1
-	go test "${tags}" "${timeout}" "${covermode}" "${coverprofile}" -failfast -run='TestGoroutineLeak' --manual=true && write_coverage || return 1
-	go test "${tags}" "${timeout}" "${covermode}" "${coverprofile}" -failfast -run='TestGetMore' --manual=true && write_coverage || return 1
+	go test $tags $timeout $covermode $coverprofile -failfast -run='TestBigKeyValuePairs$' --manual=true && write_coverage || return 1
+	go test $tags $timeout $covermode $coverprofile -failfast -run='TestPushValueLogLimit' --manual=true && write_coverage || return 1
+	go test $tags $timeout $covermode $coverprofile -failfast -run='TestKeyCount' --manual=true && write_coverage || return 1
+	go test $tags $timeout $covermode $coverprofile -failfast -run='TestIteratePrefix' --manual=true && write_coverage || return 1
+	go test $tags $timeout $covermode $coverprofile -failfast -run='TestIterateParallel' --manual=true && write_coverage || return 1
+	go test $tags $timeout $covermode $coverprofile -failfast -run='TestBigStream' --manual=true && write_coverage || return 1
+	go test $tags $timeout $covermode $coverprofile -failfast -run='TestGoroutineLeak' --manual=true && write_coverage || return 1
+	go test $tags $timeout $covermode $coverprofile -failfast -run='TestGetMore' --manual=true && write_coverage || return 1
 
 	echo "==> DONE manual tests"
 }
@@ -69,7 +69,7 @@ root() {
 	# go test -timeout=25m -v -race github.com/dgraph-io/badger/v4/...
 
 	echo "==> Running root level tests."
-	go test "${tags}" -v -race -parallel=16 -timeout=25m -failfast "${covermode}" "${coverprofile}" . && write_coverage || return 1
+	go test $tags -v -race -parallel=16 -timeout=25m -failfast $covermode $coverprofile . && write_coverage || return 1
 	echo "==> DONE root level tests"
 }
 
@@ -77,13 +77,13 @@ stream() {
 	set -eo pipefail
 	pushd badger
 	baseDir=$(mktemp -d -p .)
-	./badger benchmark write -s --dir="${baseDir}"/test | tee "${baseDir}"/log.txt
-	./badger benchmark read --dir="${baseDir}"/test --full-scan | tee --append "${baseDir}"/log.txt
-	./badger benchmark read --dir="${baseDir}"/test -d=30s | tee --append "${baseDir}"/log.txt
-	./badger stream --dir="${baseDir}"/test -o "${baseDir}/test2" | tee --append "${baseDir}"/log.txt
-	count=$(cat "${baseDir}/log.txt" | grep "at program end: 0 B" | wc -l)
-	rm -rf "${baseDir}"
-	if [[ ${count} -ne 4 ]]; then
+	./badger benchmark write -s --dir=$baseDir/test | tee $baseDir/log.txt
+	./badger benchmark read --dir=$baseDir/test --full-scan | tee --append $baseDir/log.txt
+	./badger benchmark read --dir=$baseDir/test -d=30s | tee --append $baseDir/log.txt
+	./badger stream --dir=$baseDir/test -o "$baseDir/test2" | tee --append $baseDir/log.txt
+	count=$(cat "$baseDir/log.txt" | grep "at program end: 0 B" | wc -l)
+	rm -rf $baseDir
+	if [ $count -ne 4 ]; then
 		echo "LEAK detected in Badger stream."
 		return 1
 	fi
@@ -93,7 +93,7 @@ stream() {
 }
 
 write_coverage() {
-	if [[ ${CI} == "true" ]]; then
+	if [[ $CI == "true" ]]; then
 		if [[ -f cover_tmp.out ]]; then
 			sed -i '1d' cover_tmp.out
 			cat cover_tmp.out >>cover.out && rm cover_tmp.out
