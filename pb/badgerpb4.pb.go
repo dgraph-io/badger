337bd05c69f411e6eac14831c8e0130479f18c72
Matthew McNeely <matthew.mcneely@gmail.com>
2025-12-10 16:48:05 -0500

    Revert fmt'ed proto file

--- pb/badgerpb4.pb.go
+++ pb/badgerpb4.pb.go
@@ -13,11 +13,10 @@
 package pb
 
 import (
-	reflect "reflect"
-	sync "sync"
-
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
+	reflect "reflect"
+	sync "sync"
 )
 
 const (
